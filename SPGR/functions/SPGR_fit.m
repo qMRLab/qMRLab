a1b18f3618f618d21c70b9a8f51511a51c1b2dca
Tanguy Duval <duvaltanguy@gmail.com>
2016-09-06 10:15:50 -0400

    More robust

--- SPGR/functions/SPGR_fit.m
+++ SPGR/functions/SPGR_fit.m
@@ -16,12 +16,12 @@ function Fit = SPGR_fit(MTdata, Prot, FitOpt )
 % analysis, and visualization. Concepts Magn. Reson.. doi: 10.1002/cmr.a.21357
 % ----------------------------------------------------------------------------------------------------
 
-if isempty(Prot.Sf)
-    errordlg('Please build Sf Table in the options panel to enable fitting...')
+if ~isfield(Prot,'Sf') || isempty(Prot.Sf)
+    errordlg('Please build Sf Table in the options panel to enable fitting...'); 
 end
 
 if length(Prot.Angles)~=length(MTdata)
-    errordlg(['You set a protocol with ' num2str(length(Prot.Angles)) ' offsets/angles but your input has ' num2str(length(MTdata)) ' MTdata']);
+    errordlg(['You set a protocol with ' num2str(length(Prot.Angles)) ' offsets/angles but your input has ' num2str(length(MTdata)) ' MTdata']); 
 end
 
 % Apply B1map
@@ -37,7 +37,7 @@ end
 
 % Use R1map
 if (isfield(FitOpt,'R1') && ~isempty(FitOpt.R1) && FitOpt.R1map)
-    FitOpt.R1 = max(eps,FitOpt.R1);
+    FitOpt.R1 = max(0.1,FitOpt.R1);
     FitOpt.fx(3) = 1;
     FitOpt.st(3) = FitOpt.R1;
 end
