5a7435be29f1b9fce3cb300fb423c2f1c177dabc
Pascale Beliveau <pascale.beliveau.ca@gmail.com>
2017-03-01 15:07:10 -0500

    manage missing default files

--- Common/LoadDefaultOptions.m
+++ Common/LoadDefaultOptions.m
@@ -4,10 +4,27 @@ function LoadDefaultOptions(PathName)
 % Written by: Jean-François Cabana, 2016
 % ----------------------------------------------------------------------------------------------------
 
+CompleteFileName = fullfile(PathName, 'DefaultSim.mat');
+if exist(CompleteFileName)
+    Sim    =  load(CompleteFileName);
+else 
+    errordlg('Missing DefaultSim.mat file in Parameters folder'); return;
+end
 
-Sim    =  load(fullfile(PathName, 'DefaultSim.mat'));
-Prot   =  load(fullfile(PathName, 'DefaultProt.mat'));
-FitOpt =  load(fullfile(PathName, 'DefaultFitOpt.mat'));
+
+CompleteFileName = fullfile(PathName, 'DefaultProt.mat');
+if exist(CompleteFileName)
+    Prot   =  load(CompleteFileName);
+else 
+    errordlg('Missing DefaultProt.mat file in Parameters folder'); return;
+end
+
+CompleteFileName = fullfile(PathName, 'DefaultFitOpt.mat');
+if exist(CompleteFileName)
+    FitOpt =  load(CompleteFileName);
+else
+    errordlg('Missing DefaultFitOpt.mat file in Parameters folder'); return;
+end
 
 setappdata(0, 'Sim',    Sim);
 setappdata(0, 'Prot',   Prot);
