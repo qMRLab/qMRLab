978c7486ef489722ef42266378673e5bd4f1584e
Ian Gagnon <ian.gagnon7@gmail.com>
2017-07-07 15:25:25 -0400

    New functions!

---
+++ Addons/SimVaryRnd/SimVaryRnd.m
@@ -0,0 +1,69 @@
+function SimRndResults = SimVaryRnd(obj,Sim, Prot, FitOpt, SimRndOpt, RndParam, Method)
+%VaryRndParam Multi Voxel simulation of normally distributed parameters
+
+Param = Sim.Param;
+n = SimRndOpt.NumVoxels;
+
+
+% TODO: SimRndResults.MTdata
+fields = obj.xnames;
+for ii = 1:length(fields)
+    SimRndResults.(fields{ii}) = zeros(n,1);
+end
+
+% Create waitbar
+h = waitbar(0, sprintf('Data 0/%0.0f',n), 'Name', 'Simulating data',...
+    'CreateCancelBtn', 'setappdata(gcbf,''canceling'',1)');
+setappdata(h,'canceling',0)
+setappdata(0,'Cancel',0);
+
+tic;
+for ii = 1:n
+    Param.F   =  RndParam.F(ii);        Param.kr  =  RndParam.kr(ii);
+    Param.kf  =  Param.kr.*Param.F;     Param.R1f =  RndParam.R1f(ii);
+    Param.R1r =  RndParam.R1r(ii);      Param.T1f =  1/Param.R1f;
+    Param.T1r =  1/Param.R1r;           Param.T2f =  RndParam.T2f(ii);
+    Param.T2r =  RndParam.T2r(ii);      Param.R2f =  1/Param.T2f;
+    Param.R2r =  1/Param.T2r;           Param.M0f =  RndParam.M0f(ii);
+    Param.M0r =  Param.M0f * Param.F;   Param.G   =  computeG(0,Param.T2r, Param.lineshape);
+    Sim.Param = Param;
+    
+    switch Method
+        case 'SIRFSE';  M = SIRFSE_sim(Sim, Prot);
+        case 'bSSFP';   M = bSSFP_sim(Sim, Prot, 1);
+        case 'SPGR';    M = SPGR_sim(Sim, Prot, 1);
+    end  
+    
+    if (getappdata(0, 'Cancel'));  break;   end
+    
+    SimRndResults.MTdata(ii,:) = M;
+    
+    if (Sim.Opt.AddNoise)
+        MTdata = noise( M, Sim.Opt.SNR );
+    else
+        MTdata = M;
+    end
+    
+    if (FitOpt.R1map)
+        FitOpt.R1 = computeR1obs(Param);
+    end
+    
+    switch Method
+        case 'SIRFSE';  Fit = SIRFSE_fit(MTdata, Prot, FitOpt);
+        case 'bSSFP';   Fit = bSSFP_fit(MTdata, Prot, FitOpt);
+        case 'SPGR';    Fit = SPGR_fit(MTdata, Prot, FitOpt );
+    end
+    
+    for jj = 1:length(fields)ç
+        SimRndResults.(fields{jj})(ii) = Fit.(fields{jj});
+    end
+        
+    % Update waitbar
+    if getappdata(h,'canceling');  break;  end
+    waitbar(ii/n,h,sprintf('Data %0.0f/%0.0f',ii,n));
+end
+
+delete(h);
+SimRndResults.time = toc
+SimRndResults.fields = fields;
+end
---
+++ Common/tools/genvarname_v2.m
@@ -0,0 +1,11 @@
+function str = genvarname_v2(str)
+str = strrep(str,'#','N');
+str = strrep(str,' ','');
+str = strrep(str,'=','');
+str = strrep(str,'-','');
+str = strrep(str,'(','');
+str = strrep(str,')','');
+
+
+
+
