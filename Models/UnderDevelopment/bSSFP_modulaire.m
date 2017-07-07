c185d80cfd2f55dcbcacd0a8fc64424d381142b6
Ian Gagnon <ian.gagnon7@gmail.com>
2017-07-07 15:55:55 -0400

    New: Last version of MODULAIRE qMT techniques with small modifications in their associated functions

--- Models/UnderDevelopment/bSSFP_modulaire.m
+++ Models/UnderDevelopment/bSSFP_modulaire.m
@@ -1,6 +1,6 @@
 classdef bSSFP_modulaire
 % ----------------------------------------------------------------------------------------------------
-% bSSFP :  FILL
+% bSSFP : qMT using Balanced Steady State Free Precession acquisition
 % ----------------------------------------------------------------------------------------------------
 % Assumptions :
 % (1) FILL
@@ -46,61 +46,62 @@ classdef bSSFP_modulaire
         fx           = [ 0       0      1        1        0       0 ]; % fix parameters
         
         % Protocol
+        % You can define a default protocol here.
         Prot = struct('MTdata',...
                                struct('Format',{{'Alpha' 'Trf'}},...
                                       'Mat', [5 2.7e-4; 10 2.7e-4; 15 2.7e-4; 20 2.7e-4; 25 2.7e-4 
                                               30 2.7e-4; 35 2.7e-4; 40 2.7e-4; 35 2.3e-4; 35 3.0e-4
                                               35 4.0e-4; 35 5.8e-4; 35 8.4e-4; 35 0.0012; 35 0.0012 
                                               35 0.0021]));
-                                  % You can define a default protocol here.
-                   
-   %                                
-                                  
+                                      
         % Model options
         buttons = {'G(0)',1.2524e-05,...
                    'Use R1map to constrain R1f',false,...
                    'Fix R1r = R1f',true,...
                    'Fix TR',false,...
                    'TR Value', 0.00519,...
-                   'PANEL',2,'Inversion Pulse',1,...
-                   'Shape',{'hard','gaussian','gausshann','sinc','sinchann','sincgauss','fermi'},'# of RF pulses', 500};
-%                    'Fix TR - Trf',true,...
-%                    'TR - Trf Value', 0.00269,...
-%                    'Alpha/2 - TR/2 PREPULSE',true};
+                   'PANEL','Inversion_Pulse',2,...
+                   'Shape',{'hard','gaussian','gausshann','sinc','sinchann','sincgauss','fermi'},'# of RF pulses', 500,...
+                   'Fix TR - Trf',true,...
+                   'TR - Trf Value', 0.00269,...
+                   'Prepulse',true}; % (Alpha/2 - TR/2 )
         options= struct(); % structure filled by the buttons. Leave empty in the code
         
     end
     
     methods
         function obj = bSSFP_modulaire
-            obj = button2opts(obj);
+            obj.options = button2opts(obj.buttons);
+            obj = UpdateFields(obj);
         end
         
         function obj = UpdateFields(obj)
-%             if obj.options.FixTR0x2DTrf
-%                 obj.options.FixTR = false;
-%             end
-        end
-        
-        function Smodel = equation(obj, x)
+            if obj.options.FixTRTrf
+                obj.options.FixTR = false;
+            end
+            if obj.options.UseR1maptoconstrainR1f
+                obj.fx(3)=true;
+            end
         end
         
         function FitResults = fit(obj,data)
-            if isfield(data,'R1map'), FitOpt.R1 = data.R1map; end
-            FitOpt.names = obj.xnames;
-            FitOpt.fx = obj.fx;
-            FitOpt.st = obj.st;
-            FitOpt.lb = obj.lb;
-            FitOpt.ub = obj.ub;
-            FitOpt.R1reqR1f = obj.options.FixR1r0x3DR1f;
-            FitOpt.G = obj.options.G0x2800x29;
-            Protocol = Prot2Protocol(obj);                            
+            Protocol = GetProt(obj); 
+            FitOpt = GetFitOpt(obj,data);
             FitResults = bSSFP_fit(data.MTdata,Protocol,FitOpt);                  
         end
         
-%         function plotmodel(obj, x, data)
-%           
-%         end
+        function plotmodel(obj, x, data)
+            Protocol = GetProt(obj);
+            FitOpt = GetFitOpt(obj,data);
+            SimCurveResults = bSSFP_SimCurve(x, Protocol, FitOpt );
+            Sim.Opt.AddNoise = 0;
+            axe(1) = subplot(2,1,1);
+            axe(2) = subplot(2,1,2);
+            bSSFP_PlotSimCurve(data.MTdata, data.MTdata, Protocol, Sim, SimCurveResults, axe);
+            title(sprintf('F=%0.2f; kf=%0.2f; R1f=%0.2f; R1r=%0.2f; T2f=%0.2f; M0f=%0.2f; Residuals=%f', ...
+                x.F,x.kf,x.R1f,x.R1r,x.T2f,x.M0f,x.resnorm), ...
+                'FontSize',10);
+        end
         
 %         function plotProt(obj)
 %             subplot(1,1,2)
@@ -117,13 +118,25 @@ classdef bSSFP_modulaire
 %             ViewPulse(Pulse,'b1');
 %         end
 %         
-    function Protocol = Prot2Protocol(obj)  
-            Protocol.alpha = obj.Prot.MTdata.Mat(:,1);
-            Protocol.Trf = obj.Prot.MTdata.Mat(:,2);
-            Protocol.FixTR = obj.options.FixTR;
-            Protocol.TR = obj.options.TRValue;
-            Protocol.Td = obj.options.TR0x2DTrfValue;
-            Protocol.PulseShape = obj.options.Shape;
+        function Prot = GetProt(obj)  
+            Prot.alpha = obj.Prot.MTdata.Mat(:,1);
+            Prot.Trf = obj.Prot.MTdata.Mat(:,2);
+            Prot.FixTR = obj.options.FixTR;
+            Prot.TR = obj.options.TRValue;
+            Prot.Td = obj.options.TRTrfValue;
+            Prot.PulseShape = obj.options.Inversion_Pulse_Shape;
+        end
+                
+        function FitOpt = GetFitOpt(obj,data)
+            if isfield(data,'R1map'), FitOpt.R1 = data.R1map; end
+            FitOpt.R1map = obj.options.UseR1maptoconstrainR1f;
+            FitOpt.names = obj.xnames;
+            FitOpt.fx = obj.fx;
+            FitOpt.st = obj.st;
+            FitOpt.lb = obj.lb;
+            FitOpt.ub = obj.ub;
+            FitOpt.R1reqR1f = obj.options.FixR1rR1f;
+            FitOpt.G = obj.options.G0;
         end
 
     end
