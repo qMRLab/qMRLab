facc74631de92a3f60dced0ce0e4cb1134f803a7
Ian Gagnon <ian.gagnon7@gmail.com>
2017-07-20 10:38:43 -0400

    Smalls corrections: B1 becomes B1map (to fit with other functions) and

--- Models/T1 Mapping/VFA_T1.m
+++ Models/T1 Mapping/VFA_T1.m
@@ -6,8 +6,8 @@ classdef VFA_T1
 % FILL
 % ----------------------------------------------------------------------------------------------------
 %  Inputs:
-%  SPGR: spoiled Gradient echo. 4D volume with variable flip angles
-%  B1:   excitation (B1+) fieldmap. Used to correct flip angles.
+%    * SPGR  : spoiled Gradient echo. 4D volume with variable flip angles
+%    * B1map : excitation (B1+) fieldmap. Used to correct flip angles.
 %
 %  Output maps:
 %    * T1
@@ -25,7 +25,7 @@ classdef VFA_T1
 % ----------------------------------------------------------------------------------------------------
 
     properties
-        MRIinputs = {'SPGR','B1'};
+        MRIinputs = {'SPGR','B1map'};
         xnames = {};
         voxelwise = 0;
         
@@ -49,7 +49,7 @@ classdef VFA_T1
             % T1 and M0
             flipAngles = (obj.Prot.SPGR.Mat(:,1))';
             TR = obj.Prot.SPGR.Mat(1,2);
-            [FitResult.M0, FitResult.T1] = mtv_compute_m0_t1(double(data.SPGR(:,:,:,:)), flipAngles(1:length(flipAngles)), TR, data.B1);
+            [FitResult.M0, FitResult.T1] = mtv_compute_m0_t1(double(data.SPGR(:,:,:,:)), flipAngles(1:length(flipAngles)), TR, data.B1map);
        
         end
         
