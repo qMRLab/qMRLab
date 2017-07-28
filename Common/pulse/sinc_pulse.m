4e9d371adac1431e4fef708b8ed2d3e1bd3195bd
Mathieu Boudreau <emb6150@gmail.com>
2017-07-28 15:09:12 -0230

    Add reference for the sinc pulse

--- Common/pulse/sinc_pulse.m
+++ Common/pulse/sinc_pulse.m
@@ -1,10 +1,31 @@
 function pulse = sinc_pulse(t,Trf,PulseOpt)
-% sinc_pulse : Compute sinc RF pulse
+%SINC_PULSE Sinc RF pulse function.
+%   pulse = sinc_pulse(t,Trf,PulseOpt)
+%
+%   The sinc pulse is defined to be 0 outside the pulse window (before 
+%   t = 0 or after t=Trf), and follows a symmetric sinc lineshape within.
+%
+%   --args--
+%   t: Function handle variable, represents the time.
+%   Trf: Duration of the Sinc RF pulse.
+%
+%   --optional args--
+%   PulseOpt: Struct. Contains optional parameters for pulse shapes.
+%
+%             -properties-
+%             TBW: Time-bandwidth product. If TBW is an even numnber, it's
+%                  also the # of zero crossings of the RF envelope,
+%                  including the margins.
+%
+%   Reference: Matt A. Bernstein, Kevin F. Kink and Xiaohong Joe Zhou.
+%              Handbook of MRI Pulse Sequences, pp. 38, Eqs. 2.2-2.4, (2004)
+%
+
 
 if (nargin < 3); PulseOpt = struct; end
 
 if(~isfield(PulseOpt,'TBW') || isempty(PulseOpt.TBW) || ~isfinite(PulseOpt.TBW))
-    TBW = 4;    % time-bandwidth window (# of zeros)
+    TBW = 4;
 else
     TBW = PulseOpt.TBW;
 end
