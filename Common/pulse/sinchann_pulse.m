5c4e5445e5df27d40050230cd43fb01a8879f95a
Mathieu Boudreau <emb6150@gmail.com>
2017-07-28 15:42:17 -0230

    Add help doc for gausshann_pulse.m

--- Common/pulse/sinchann_pulse.m
+++ Common/pulse/sinchann_pulse.m
@@ -3,12 +3,12 @@ function pulse = sinchann_pulse(t,Trf,PulseOpt)
 %   pulse = sinchann_pulse(t,Trf,PulseOpt)
 %
 %   The sinc-hann pulse is defined to be 0 outside the pulse window (before 
-%   t = 0 or after t=Trf), follows a symmetric sinc lineshape within which  
+%   t = 0 or after t=Trf), follows a symmetric sinc lineshape within, and  
 %   is apodized by a Hanning window.
 %
 %   --args--
 %   t: Function handle variable, represents the time.
-%   Trf: Duration of the sinc-hann RF pulse.
+%   Trf: Duration of the Sinc-Hann RF pulse.
 %
 %   --optional args--
 %   PulseOpt: Struct. Contains optional parameters for pulse shapes.
@@ -19,7 +19,8 @@ function pulse = sinchann_pulse(t,Trf,PulseOpt)
 %                  including the margins.
 %
 %   Reference: Matt A. Bernstein, Kevin F. Kink and Xiaohong Joe Zhou.
-%              Handbook of MRI Pulse Sequences, pp. 39, Eq. 2.6, (2004)
+%              Handbook of MRI Pulse Sequences, pp. 38-39, Eqs. 2.2-2.4 and
+%              2.6, (2004)
 %
 %   See also GETPULSE, VIEWPULSE.
 %
