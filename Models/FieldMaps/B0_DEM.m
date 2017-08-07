acc1901d117b5dd1355b97f61bf4874bc3a23bf5
Ian Gagnon <ian.gagnon7@gmail.com>
2017-08-07 17:14:50 -0400

    NEW: Unwrapping is now done with "sunwrap" for 2D data and "laplacianUnwrap" for 3D data

--- Models/FieldMaps/B0_DEM.m
+++ Models/FieldMaps/B0_DEM.m
@@ -33,7 +33,7 @@ classdef B0_DEM
         Prot = struct('Time',struct('Format',{'deltaTE'},'Mat', 1.92e-3));
         
         % Model options
-        buttons = {};
+        buttons = {'Magn thresh lb',0};
         options= struct(); % structure filled by the buttons. Leave empty in the code
         
     end
@@ -45,17 +45,52 @@ classdef B0_DEM
         end
         
         function obj = UpdateFields(obj)
+            if obj.options.Magnthreshlb == 0
+                obj.options.Magnthreshlb = '';
+            end
         end
         
         function FitResult = fit(obj,data)
             Phase = data.Phase;
             Magn = data.Magn;
-            Complex = Magn.*exp(Phase*1i);
-            Phase_uw = Phase;
-            for it = 1:size(Magn,4)
-                Phase_uw(:,:,:,it) = sunwrap(Complex(:,:,:,it));
+            
+%             % FSL "prelude"
+%             mkdir('tmp')
+%             save_nii(make_nii(Phase),'tmp/Phase.nii');
+%             save_nii(make_nii(Magn),'tmp/Magn.nii');
+%             unix('prelude -p tmp/Phase.nii -a tmp/Magn.nii -o tmp/Ph_uw -f');
+%             B0 = load_untouch_nii('tmp/Ph_uw.nii.gz');
+%             rmdir('tmp','s')
+%             B0.img = unwrap(B0.img,[],4);
+%             FitResult.B0map = (B0.img(:,:,:,2) - B0.img(:,:,:,1))/(obj.Prot.Time.Mat*2*pi);
+            
+            
+            % 2D or 3D data ?
+            if any(size(Phase) == 1)
+                TwoD  = true;
+            else
+                TwoD = false;
             end
-            FitResult.B0map = (Phase_uw(:,:,:,2) - Phase_uw(:,:,:,1))/(obj.Prot.Time.Mat*2*pi);           
+            
+            % MATLAB "sunwrap" for 2D data
+            if TwoD
+                Complex = Magn.*exp(Phase*1i);
+                Phase_uw = Phase;
+                for it = 1:size(Magn,4)
+                    Phase_uw(:,:,:,it) = sunwrap(Complex(:,:,:,it));
+                end
+                FitResult.B0map = (Phase_uw(:,:,:,2) - Phase_uw(:,:,:,1))/(obj.Prot.Time.Mat*2*pi); 
+                     
+            % MATLAB "laplacianUnwrap" for 3D data
+            else
+                if isempty(obj.options.Magnthreshlb)
+                    msgbox('Enter a Lower Bound relative to the Magn','Create a Mask');
+                end
+                Phase_uw = laplacianUnwrap(Phase, magn>.05);
+                FitResult.B0map = (Phase_uw(:,:,:,2) - Phase_uw(:,:,:,1))/(obj.Prot.Time.Mat*2*pi);                 
+            end
+            
+            
         end        
     end
 end
