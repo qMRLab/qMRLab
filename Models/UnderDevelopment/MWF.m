45d5e68c66725d38309b7d57b478cadc90684ca9
Ian Gagnon <ian.gagnon7@gmail.com>
2017-08-24 13:20:39 -0400

    Modification: For MWF Model, instead of entering the first echo and the echo spacing, user now enter all the echo times (because the echo spacing isn't always the same).

--- Models/UnderDevelopment/MWF.m
+++ Models/UnderDevelopment/MWF.m
@@ -56,15 +56,16 @@ classdef MWF
         
         % Protocol
         % You can define a default protocol here.
-        Prot  = struct('Echo',struct('Format',{{'First (ms)'; 'Spacing (ms)'}},...
-                                     'Mat', [10; 10])); 
+        Prot  = struct('Echo',struct('Format',{{'Time (ms)'}},...
+                                     'Mat', [10; 20; 30; 40; 50; 60; 70; 80; 90; 100; 110; 120; 130; 140; 150; 160; 170; 
+                                             180; 190; 200; 210; 220; 230; 240; 250; 260; 270; 280; 290; 300; 310; 320])); 
         
         % Model options
         buttons = {'Cutoff (ms)',40, 'Sigma', 28, 'Relaxation Type' {'T2', 'T2star'}};
         options = struct(); % structure filled by the buttons. Leave empty in the code
         
         % Simulation Options
-        Sim_Single_Voxel_Curve_buttons = {'nEchoes',32};
+        Sim_Single_Voxel_Curve_buttons = {};
         Sim_Sensitivity_Analysis_buttons = {'# of run',5};
 
     end
@@ -84,8 +85,7 @@ classdef MWF
         
         function [FitResults,Spectrum] = fit(obj,data)   
             % EchoTimes, T2 and DecayMatrix
-            nEchoes     = length(data.MET2data);
-            EchoTimes   = getEchoTimes(obj,nEchoes);
+            EchoTimes   = obj.Prot.Echo.Mat;
             T2          = getT2(obj,EchoTimes);
             DecayMatrix = getDecayMatrix(EchoTimes,T2.vals);
             % Options
@@ -96,13 +96,13 @@ classdef MWF
             Opt.upper_cutoff_IEW = obj.ub(3);            
             % Fitting
             if isempty(data.Mask), data.Mask = 1; end
-            [FitResults,Spectrum] = multi_comp_fit_v2(reshape(data.MET2data,[1 1 1 nEchoes]), EchoTimes, DecayMatrix, T2, Opt, 'tissue', data.Mask);      
+            [FitResults,Spectrum] = multi_comp_fit_v2(reshape(data.MET2data,[1 1 1 length(obj.Prot.Echo.Mat)]), EchoTimes, DecayMatrix, T2, Opt, 'tissue', data.Mask);      
         end
           
         function FitResults = Sim_Single_Voxel_Curve(obj, x, Opt,display)
             % Example: obj.Sim_Single_Voxel_Curve(obj.st,button2opts(obj.Sim_Single_Voxel_Curve_buttons))
             if ~exist('display','var'), display = 1; end
-            Smodel = equation(obj,x,Opt);
+            Smodel = equation(obj,x);
             sigma = 1./Opt.SNR;
             data.MET2data = random('rician',Smodel,sigma);
             data.Mask = 1;
@@ -123,44 +123,61 @@ classdef MWF
         end
         
         function plotmodel(obj, x, data, SpectrumReg)
-            EchoTimes   = getEchoTimes(obj,length(data.MET2data));
+            EchoTimes   = obj.Prot.Echo.Mat;
             T2          = getT2(obj,EchoTimes);
-            Opt.nEchoes = length(data.MET2data);
-            [Smodel, Spectrum] = equation(obj,x,Opt);
-            
-            % Figure
+            [Smodel, Spectrum] = equation(obj,x);
+             
+            if exist('SpectrumReg','var')
+                
+            % Figure with SPECTRUM
 %----------------------- subplot 1 -----------------------%
-            subplot(2,1,1)
-            plot(T2.vals,Spectrum);
-            hold on
-            plot(T2.vals,SpectrumReg,'r');
-            hold off
-            Title           = title('Spectrums comparison');
-            Title.FontSize  = 12;
-            Legend          = legend('Simulated data','Fitted data','Location','Best');
-            Legend.FontSize = 10;
-            xlabel('T2 (ms)'); 
-            ylabel('Proton density');
+                subplot(2,1,1)
+                plot(T2.vals,Spectrum);
+                hold on
+                plot(T2.vals,SpectrumReg,'r');
+                hold off
+                Title           = title('Spectrums comparison');
+                Title.FontSize  = 12;
+                Legend          = legend('Simulated data','Fitted data','Location','Best');
+                Legend.FontSize = 10;
+                xlabel('T2 (ms)'); 
+                ylabel('Proton density');
 %----------------------- subplot 2 -----------------------%
-            subplot(2,1,2)
-            plot(EchoTimes,data.MET2data,'+')
-            hold on
-            plot(EchoTimes,Smodel,'r')
-            hold off
-            Title           = title('Fitting');
-            Title.FontSize  = 12;
-            Legend          = legend('Simulated data','Fitted curve','Location','Best');
-            Legend.FontSize = 10;
-            xlabel('EchoTimes (ms)'); 
-            ylabel('MET2 ()');
-%---------------------------------------------------------%        
+                subplot(2,1,2)
+                plot(EchoTimes,data.MET2data,'+')
+                hold on
+                plot(EchoTimes,Smodel,'r')
+                hold off
+                Title           = title('Fitting');
+                Title.FontSize  = 12;
+                Legend          = legend('Simulated data','Fitted curve','Location','Best');
+                Legend.FontSize = 10;
+                xlabel('EchoTimes (ms)'); 
+                ylabel('MET2 ()');
+%---------------------------------------------------------%
+%---------------------------------------------------------%
+
+            % Figure without SPECTRUM
+            else
+                plot(EchoTimes,data.MET2data,'+')
+                hold on
+                plot(EchoTimes,Smodel,'r')
+                hold off
+                Title           = title('Fitting');
+                Title.FontSize  = 12;
+                Legend          = legend('Simulated data','Fitted curve','Location','Best');
+                Legend.FontSize = 10;
+                xlabel('EchoTimes (ms)'); 
+                ylabel('MET2 ()');
+            end
+%---------------------------------------------------------%         
         end
         
-        function [Smodel, Spectrum] = equation(obj,x,Opt)
+        function [Smodel, Spectrum] = equation(obj,x)
             if isnumeric(x), xbu = x; x.MWF = xbu(1); x.T2MW = xbu(2); x.T2IEW = xbu(3); end
             x.MWF = x.MWF/100;
             % EchoTimes, T2 and DecayMatrix
-            EchoTimes   = getEchoTimes(obj,Opt.nEchoes);
+            EchoTimes   = obj.Prot.Echo.Mat;
             T2          = getT2(obj,EchoTimes); 
             DecayMatrix = getDecayMatrix(EchoTimes,T2.vals);
             % MF (Myelin Fraction) and IEF (Intra/Extracellular Fraction)
@@ -190,13 +207,7 @@ function T2 = getT2(obj,EchoTimes)
     end
     T2.vals = T2.range(1)*(T2.range(2)/T2.range(1)).^(0:(1/(T2.num-1)):1)';   
 end
-        
-function EchoTimes = getEchoTimes(obj,nEchoes)
-    FirstEcho   = obj.Prot.Echo.Mat(1);
-    EchoSpacing = obj.Prot.Echo.Mat(2);
-    EchoTimes	= FirstEcho + EchoSpacing*(0:nEchoes-1); 
-end
-        
+               
 function DecayMatrix = getDecayMatrix(EchoTimes,T2vals)            
     DecayMatrix = zeros(length(EchoTimes),length(T2vals));
     for j = 1:length(T2vals)
