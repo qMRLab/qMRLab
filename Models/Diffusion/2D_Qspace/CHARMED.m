a2228ded0667c0c0595552dfa786f1cc4cd19763
Tanguy Duval <duvaltanguy@gmail.com>
2017-08-31 15:47:33 -0400

    UpdateProt when loading protocol

--- Models/Diffusion/2D_Qspace/CHARMED.m
+++ Models/Diffusion/2D_Qspace/CHARMED.m
@@ -352,8 +352,12 @@ cmax = max(c); % find images repeated more than 5 times (for relevant STD)
 if cmax<5, uiwait(errordlg('Your dataset doesn''t have 5 repeated measures (same bvec/bvals) --> you can''t estimate noise STD voxel-wise. Specify a fixed Sigma Noise in the option panel instead. (see scd_noise_fit_histo_nii.m to estimate the noise STD).')); end
 
 repeated_measured = find(ismember(c,c(c>5)));
-for irep = 1:length(repeated_measured)
-    vars(irep) = var(data(ind==repeated_measured(irep)));
+if ~isempty(repeated_measured)
+    for irep = 1:length(repeated_measured)
+        vars(irep) = var(data(ind==repeated_measured(irep)));
+    end
+    SigmaNoise = sqrt(median(vars));
+else
+    SigmaNoise=0;
 end
-SigmaNoise = sqrt(median(vars));
 end
