34f83b316c8e7a2b4722656f8bfc34b4ab7b7815
Tanguy DUVAL <duvaltanguy@gmail.com>
2017-09-12 11:24:58 -0400

    small corrections

--- Test/MoxUnitCompatible/test_Models.m
+++ Test/MoxUnitCompatible/test_Models.m
@@ -31,8 +31,8 @@ for im = 1:length(MethodList)
     Smodel = Model.equation(st);
     % compare with Ground Truth
     GT = load(['value_' class(Model) '.mat']);
-    assertEqual(Smodel,GT.Smodel)
-    assertEqual(st,GT.st)
+    assertVectorsAlmostEqual(Smodel,GT.Smodel,'relative',1e-2)
+    assertVectorsAlmostEqual(st,GT.st,'relative',1e-4)
 end
 
 
