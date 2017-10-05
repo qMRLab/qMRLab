8fbe2e4d359d72a1f8bffff5dfcacafd673c68f8
Tanguy DUVAL <duvaltanguy@gmail.com>
2017-10-05 16:59:22 -0400

    Testing all options

--- Common/tools/button2opts.m
+++ Common/tools/button2opts.m
@@ -1,4 +1,5 @@
-function options = button2opts(opts)
+function options = button2opts(opts,all)
+if ~exist('all','var'), all=false; end % output all possible options
 io=1;
 options = struct();
 while io < length(opts)
@@ -7,24 +8,43 @@ while io < length(opts)
         io = io+3;
         for ii=1:panelNum            
             if iscell(opts{io+1})
-                options.(genvarname_v2([PanelName '_' opts{io}])) = opts{io+1}{1};
+                [options(:).(genvarname_v2([PanelName '_' opts{io}]))] = deal(opts{io+1}{1});
+                if all
+                    for iall=2:length(opts{io+1})
+                        options(end+1) = options(1);
+                        options(end).(genvarname_v2([PanelName '_' opts{io}]))=opts{io+1}{iall};
+                    end
+                end
                 io = io+2;
             elseif strcmp(opts{io+1},'pushbutton')
-                options.(genvarname_v2([PanelName '_' opts{io}])) = false;
+                [options(:).(genvarname_v2([PanelName '_' opts{io}]))] = deal(false);
                 io = io+2;
             else
-                options.(genvarname_v2([PanelName '_' opts{io}])) = opts{io+1};
+                [options(:).(genvarname_v2([PanelName '_' opts{io}]))] = deal(opts{io+1});
+                if all && islogical(opts{io+1})
+                    options(end+1) = options(1);
+                    options(end).(genvarname_v2([PanelName '_' opts{io}]))=~opts{io+1}; 
+                end
                 io = io+2;
             end
         end
     else
         if iscell(opts{io+1})
-            options.(genvarname_v2(opts{io})) = opts{io+1}{1};
+            [options(:).(genvarname_v2(opts{io}))] = deal(opts{io+1}{1});
+            if all
+                for iall=2:length(opts{io+1})
+                    options(end+1) = options(1);
+                    options(end).(genvarname_v2(opts{io}))=opts{io+1}{iall};
+                end
+            end
         elseif strcmp(opts{io+1},'pushbutton')
-            options.(genvarname_v2(opts{io})) = false;
-            io = io+2;
+            [options(:).(genvarname_v2(opts{io}))] = deal(false);
         else
-            options.(genvarname_v2(opts{io})) = opts{io+1};
+            [options(:).(genvarname_v2(opts{io}))] = deal(opts{io+1});
+            if all && islogical(opts{io+1})
+                options(end+1) = options(1);
+                options(end).(genvarname_v2(opts{io}))=~opts{io+1};
+            end
         end
         io = io+2;
     end
--- Test/MoxUnitCompatible/test_Models.m
+++ Test/MoxUnitCompatible/test_Models.m
@@ -27,12 +27,27 @@ for im = 1:length(MethodList)
     Model = str2func(MethodList{im}); Model = Model();
     if ~Model.voxelwise, continue; end
     disp(class(Model))
-    try st = Model.st; catch, try st = mean([Model.lb(:),Model.ub(:)],2); catch, st = ones(length(Model.xnames),1); end; end
-    Smodel = Model.equation(st);
-    % compare with Ground Truth
-    GT = load(['value_' class(Model) '.mat']);
-    assertVectorsAlmostEqual(st,GT.st,'relative',1e-4,[MethodList{im} ' changed... value_' MethodList{im} '.mat has to be regenerated'])
-    assertVectorsAlmostEqual(Smodel,GT.Smodel,'relative',1e-2,['Testing consistency of equation for Model ' MethodList{im}])
+    ModelOpt = button2opts(Model.buttons,1);
+    clear st Smodel
+    for iopt=1:length(ModelOpt) % try all model options
+        Model.options = ModelOpt(iopt);
+        disp(['Testing ' class(Model) ' option:'])
+        disp(Model.options)
+        Model = Model.UpdateFields;
+        try st{iopt} = Model.st; catch, try st{iopt} = mean([Model.lb(:),Model.ub(:)],2); catch, st{iopt} = ones(length(Model.xnames),1); end; end
+        Smodel{iopt} = Model.equation(st{iopt});
+        % CHECK CONSITENSY WITH PREVIOUS VERSIONS:
+        if exist(['value_' class(Model) '.mat'],'file')
+            % compare with Ground Truth
+            GT = load(['value_' class(Model) '.mat']);
+            [~,ModelOpttest,GTModelOpttest]=comp_struct(ModelOpt,GT.ModelOpt);
+            assertTrue(isempty(ModelOpttest) & isempty(GTModelOpttest),evalc('ModelOpttest, GTModelOpttest'))
+            assertVectorsAlmostEqual(st{iopt},GT.st{iopt},'relative',1e-4,[MethodList{im} ' changed... value_' MethodList{im} '.mat has to be regenerated'])
+            assertVectorsAlmostEqual(Smodel{iopt},GT.Smodel{iopt},'relative',1e-2,['Testing consistency of equation for Model ' MethodList{im}])
+        elseif iopt == length(ModelOpt)
+            save(['value_' class(Model) '.mat'],'Smodel','st','ModelOpt')
+        end
+    end
 end
 
 
@@ -43,15 +58,18 @@ for im = 1:length(MethodList)
     Model = str2func(MethodList{im}); Model = Model();
     if ~Model.voxelwise, continue; end
     disp(class(Model))
-    try Opt = button2opts(Model.Sim_Single_Voxel_Curve_buttons); end
+    try Opt = button2opts(Model.Sim_Single_Voxel_Curve_buttons,1); end
     try st = Model.st; catch, try st = mean([Model.lb(:),Model.ub(:)],2); catch, st = ones(length(Model.xnames),1); end; end
-    Opt.SNR = 1000;
-    FitResults = Model.Sim_Single_Voxel_Curve(st,Opt);
-    
-    % Compare inputs and outputs
-    fnm=fieldnames(FitResults);
-    FitResults = rmfield(FitResults,fnm(~ismember(fnm,Model.xnames))); fnm=fieldnames(FitResults);
-    [~,FitResults,GroundTruth]=comp_struct(FitResults,mat2struct(st,Model.xnames),[],[],.20);
-    assertTrue(isempty(FitResults) & isempty(GroundTruth),evalc('FitResults, GroundTruth'))
+    [Opt(:).SNR] = deal(1000);
+    for iopt=1:length(Opt) % Test all simulation options
+        disp(['Testing ' class(Model) ' simulation option:'])
+        disp(Opt(iopt))
+        FitResults = Model.Sim_Single_Voxel_Curve(st,Opt(iopt));
+        % Compare inputs and outputs
+        fnm=fieldnames(FitResults);
+        FitResults = rmfield(FitResults,fnm(~ismember(fnm,Model.xnames))); fnm=fieldnames(FitResults);
+        [~,FitResults,GroundTruth]=comp_struct(FitResults,mat2struct(st,Model.xnames),[],[],.50);
+        assertTrue(isempty(FitResults) & isempty(GroundTruth),evalc('FitResults, GroundTruth'))
+    end
     disp ..ok
 end
