e6acd5135700be46c1c6b36657559ee91d33e3b0
Ilana Leppert <ilana@ilana-macbook.local>
2017-10-24 09:42:14 -0400

    nicer neuropoly logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -246,7 +246,8 @@ html_theme_options = {
     'display_version': False,
     'navigation_depth': 4,
 }
-html_logo = '_static/logo-neuropoly.png'
+#html_logo = '_static/logo-neuropoly.png'
+html_logo = '_static/neuropoly_logo-try.png'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
