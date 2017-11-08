307f24edd461450e21e4ba0222e1119370911c8d
Tanguy Duval <duvaltanguy@gmail.com>
2017-11-08 18:59:04 -0500

    revert GetSf (problem on Octave, although it was much faster on Matlab)

--- Models_Functions/SPGRfun/functions/GetSf.m
+++ Models_Functions/SPGRfun/functions/GetSf.m
@@ -1,12 +1,15 @@
 function Sfi = GetSf(angles, offsets, T2f, SfTable)
 %GetSf interpolate Sf values from precomputed table
 
-Sfi = interp3(SfTable.offsets, SfTable.angles, SfTable.T2f, SfTable.values, offsets, angles, T2f*ones(length(angles),1));
+Sfi = zeros(length(angles),1);
 
-nanindex = find(isnan(Sfi));
-if ~isempty(nanindex)
-    for ii = nanindex
-        warning('Cannot interpolate value from current Sf table : angle: %f; offset: %f; T2f: %f\n', angles(ii), offsets(ii), T2f);
+for ii = 1:length(angles)
+   
+[xi, yi, zi] = meshgrid(offsets(ii), angles(ii), T2f);
+Sfi(ii) = interp3(SfTable.offsets, SfTable.angles, SfTable.T2f, SfTable.values, xi, yi, zi);
+
+    if (isnan(Sfi(ii)))
+        warning('Cannot interpolate value from current Sf table : angle: %f; offset: %f; T2f: %f\n', angles(ii), offsets(ii), T2f); 
         disp('Computing missing Sf value...');
         MTpulse = GetPulse(angles(ii),offsets(ii),SfTable.PulseTrf,SfTable.PulseShape,SfTable.PulseOpt);
         Sfi(ii) = computeSf(T2f, MTpulse);
