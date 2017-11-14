4749ec5648b7df1b36af953a933c2282aa9e7df8
Tanguy DUVAL <duvaltanguy@gmail.com>
2017-11-14 11:12:10 -0500

    new batch examples

--- Data/MTSAT_demo/MTSAT_batch.m
+++ Data/MTSAT_demo/MTSAT_batch.m
@@ -33,7 +33,7 @@ delete('FitTempResults.mat');
 % .MAT file : FitResultsSave_mat(FitResults,folder);
 % .NII file : FitResultsSave_nii(FitResults,fname_copyheader,folder);
 FitResultsSave_nii(FitResults,'MTw.nii.gz');
-save('CHARMEDParameters.mat','Model');
+qMRsaveModel(Model,'MTsat.qMRLab.mat'); % by convention use extension .qMRLab.mat
 
 %% Check the results
 % Load them in qMRLab
