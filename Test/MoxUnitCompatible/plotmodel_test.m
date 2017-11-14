c299d16572b9f9b073e87c2e59bba49f47ded981
Tanguy Duval <duvaltanguy@gmail.com>
2017-11-14 17:05:03 -0500

    still some issues with load.nii and octave.. Changing .travis.yml temporary for testing

--- Test/MoxUnitCompatible/plotmodel_test.m
+++ Test/MoxUnitCompatible/plotmodel_test.m
@@ -1,4 +1,4 @@
-function test_suite=test_Models
+function test_suite=plotmodel_test
 try % assignment of 'localfunctions' is necessary in Matlab >= 2016
     test_functions=localfunctions();
 catch % no problem; early Matlab versions can use initTestSuite fine
