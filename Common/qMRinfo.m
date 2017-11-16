721767f2243a6d64990a54b1045dc1d9241ea4ab
Tanguy Duval <duvaltanguy@gmail.com>
2017-11-16 15:08:59 -0500

    document scripts

--- Common/qMRinfo.m
+++ Common/qMRinfo.m
@@ -1,5 +1,9 @@
 function varargout = qMRinfo(varargin)
+% Display Model headers (MR protocol, model detail, fitting parameters, options)
+% Example:
+%   qMRinfo('MWF'); % display help for Myelin Water Fraction
 if ~moxunit_util_platform_is_octave
+    if nargin==0, varargin{1}='qMRinfo'; end
     [varargout{1:nargout}] = help(varargin{:});
 else
     varargout{1} = 'Sorry, not implemented for Octave yet. Please contribute on github.com/neuropoly/qMRLab';
