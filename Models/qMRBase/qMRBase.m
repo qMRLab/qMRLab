b1bfe709401cbeed4a1ed30b7fa65e6bc44647d6
agahkarakuzu <agahkarakuzu@gmail.com>
2017-11-22 15:59:26 -0500

    Adding template for an abstract base class

---
+++ Models/qMRBase/qMRBase.m
@@ -0,0 +1,39 @@
+classdef (Abstract) qMRBase
+    
+properties
+       
+
+
+    
+    
+end
+
+methods
+
+function obj = loadobj(input_vars)
+
+
+end
+
+
+function saveobj(obj)
+
+s = struct();
+% s.X = obj().X
+
+end
+
+
+
+end
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+end
