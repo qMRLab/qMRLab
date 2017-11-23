be1c04cdb2e88151cc03df60668df38b0c7fb04b
Mathieu Boudreau <emb6150@gmail.com>
2017-11-23 18:17:20 -0330

    Add save/load methods to models (saves object vars) & test that save/load returns same object

--- Models/AbstractModel.m
+++ Models/AbstractModel.m
@@ -1,11 +1,31 @@
 classdef (Abstract) AbstractModel
 % AbstractModel:  Properties/Methods shared between all models.
 %
+%   *Methods*
+%   save: save model to a file as an object variable.
+%   load: load model from a file as an object variable.
+%
 
 properties
 end
 
 methods
+    function saveObj(obj, fileName)
+        try
+            save(fileName, 'obj', '-mat');
+        catch ME
+            error(ME.identifier, ME.message)
+        end
+    end
+    
+    function loadObj(obj, fileName)
+        try
+            tmp = load(fileName, '-mat');
+            obj = tmp.obj;
+        catch ME
+        	error(ME.identifier, ME.message)
+        end
+    end
 end
 
 end
---
+++ Test/Models/AbstractModel_Test.m
@@ -0,0 +1,44 @@
+classdef (TestTags = {'Unit'}) AbstractModel_Test < matlab.unittest.TestCase
+
+    properties
+        tempFileName = 'tmp.mat'
+        modelObject
+    end
+    
+    methods (TestClassSetup)
+         function initParPool(testCase)
+           B0_DEM_batch;
+           testCase.modelObject = Model;
+        end
+    end
+    
+    methods (TestClassTeardown)
+        function clearTempFiles(testCase)
+            if exist(testCase.tempFileName, 'file') == 2
+                delete(testCase.tempFileName)
+            end
+        end
+        
+        function guiCleanup(testCase)
+            close all
+        end
+    end
+ 
+    methods (Test)
+        function test_save_load_creates_identical_original_object(testCase)
+            %% Prep
+            originalObject = testCase.modelObject;
+            
+            %% Save
+            testCase.modelObject.saveObj(testCase.tempFileName);
+            
+            %% Load
+            loadedObject = testCase.modelObject;
+            loadedObject.loadObj(testCase.tempFileName);
+            
+            %% Test
+            assertEqual(testCase, originalObject, loadedObject);
+        end     
+    end
+    
+end
