9a48f840237f82f411ad02f57fb0e4e429ebbd09
Tanguy Duval <duvaltanguy@gmail.com>
2017-11-29 19:50:16 -0500

    Document and clean

--- Test/MoxUnitCompatible/loadModel_Test.m
+++ Test/MoxUnitCompatible/loadModel_Test.m
@@ -11,20 +11,21 @@ function load_test
 MethodList = list_models;
 
 for im = 1:length(MethodList)
+   % LOAD
    savedModel_fname = fullfile(fileparts(which('qMRLab')),'Test','MoxUnitCompatible','static_savedModelsforRetrocompatibility',[MethodList{im} '.qmrlab.mat']);
    savedProps = load(savedModel_fname);
+   % CREATE NEW OBJECT
    eval(['newModel = ' savedProps.ModelName ';']);
-  
    newProps = objProps2struct(newModel);
 
-   % Saved method is a struct with properties only.
-   
+   % TEST MISMATCH OF PROPERTIES:
    [~,LoadedModelextra,NewModelextra] = comp_struct(savedProps,newProps,3,0,inf);
-%    idx1 = ismember(newProps,savedProps);
-%    idx2 = ismember(savedProps,newProps);
    
+   % CONVERT MISMATCH 2 STRING (TEXT MESSAGES)
    newMsg = evalc('NewModelextra');
    loadMsg = evalc('LoadedModelextra');
+   
+   % REPORT MISMATCH
    if ~isempty(LoadedModelextra)
        error(['Missing field in ' MethodList{im} ' : ' newMsg loadMsg]); 
    end
