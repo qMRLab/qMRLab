2a6b954b405f43c59f9c4776dcedefd0d717e009
Tanguy Duval <duvaltanguy@gmail.com>
2017-12-01 20:01:52 -0500

    [TEST] Update Batch Example Test for the new qMRgenBatch

--- Test/MoxUnitCompatible/BatchExample_test.m
+++ Test/MoxUnitCompatible/BatchExample_test.m
@@ -9,29 +9,27 @@ function TestSetup
 setenv('ISTRAVIS','1') % go faster! Fit only 2 voxels in FitData.m
 
 function test_batch
-curdir = pwd;
-disp('testing batchExamples... (FAILURE MIGHT CORRESPOND TO COMPATIBILITY PROBLEM (e.g. variable name changed), edit qMRpatch.m ). ')
-BatchDir = [fileparts(which('qMRLab.m')) filesep 'Data'];
-[BatchList, pathmodels] = sct_tools_ls([BatchDir filesep '*batch.m'],0,0,2,1);
-for im = 1:length(BatchList)
-    cd(pathmodels{im})
+tmpDir = tempname;
+mkdir(tmpDir);
+cd(tmpDir)
+setenv('ISTRAVIS','1')
+Modellist = list_models';
+for iModel = 1:length(Modellist)
     disp('===============================================================')
-    disp(['Testing: ' BatchList{im} '...'])
+    disp(['Testing: ' Modellist{iModel} '...'])
     disp('===============================================================')
-    tmpDir = tempname;
-    mkdir(tmpDir);
-    copyfile(fullfile(pathmodels{im}(1:end-1),'*'),tmpDir)
-    cd(tmpDir)
-    if not(strcmp(BatchList{im},'NoiseLevel_batch')) && not(strcmp(BatchList{im},'CHARMED_batch')) && not(strcmp(BatchList{im},'NODDI_batch')) && not(strcmp(BatchList{im},'MTV_batch'))
-    eval(BatchList{im})
-    end
-    if moxunit_util_platform_is_octave
-        confirm_recursive_rmdir(0); % for octave
-    end
-    rmdir(tmpDir,'s')
-    disp ..ok
+
+    eval(['Model = ' Modellist{iModel}]);
+    qMRgenBatch(Model,pwd)
+    
+    
+    eval([Modellist{iModel} '_batch'])
+    
+    cd ..
 end
+setenv('ISTRAVIS','')
 cd(curdir)
+rmdir(tmpDir,'s')
 
 function TestTeardown
 setenv('ISTRAVIS','0')
