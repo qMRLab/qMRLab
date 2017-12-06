f915ea4ab8e625a4368dbd5f03e37feaf9eb97d6
Tanguy Duval <duvaltanguy@gmail.com>
2017-12-06 16:14:50 -0500

    enable SimVary canceling

--- Addons/SimVary/SimVaryPlot.m
+++ Addons/SimVary/SimVaryPlot.m
@@ -5,8 +5,11 @@ Xmax =  SimVaryResults.(Xaxis).x(end);
 Xmin = Xmin - (Xmax-Xmin)/50;
 Xmax = Xmax + (Xmax-Xmin)/50;
 X    =  SimVaryResults.(Xaxis).x;
-Y    =  SimVaryResults.(Xaxis).(Yaxis).mean;
-E    =  SimVaryResults.(Xaxis).(Yaxis).std;
+% Pad if canceled
+Y = nan(size(X));
+E = nan(size(X));
+Y(1:length(SimVaryResults.(Xaxis).(Yaxis).mean))    =  SimVaryResults.(Xaxis).(Yaxis).mean;
+E(1:length(SimVaryResults.(Xaxis).(Yaxis).std))    =  SimVaryResults.(Xaxis).(Yaxis).std;
 
 cla;
 hold on;
