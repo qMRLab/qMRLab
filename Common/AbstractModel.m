774bde396e74536a4293053f93e5274b3febfc34
Ilana LEPPERT <ilana.leppert@mcgill.ca>
2018-02-14 17:05:24 -0500

    add error-catching function

--- Common/AbstractModel.m
+++ Common/AbstractModel.m
@@ -6,37 +6,37 @@ classdef (Abstract) AbstractModel
     %   save: save model object properties to a file as an struct.
     %   load: load model properties from a struct-file into the object.
     %
-    
+
     properties
         version
         ModelName
     end
-    
+
     methods
         % Constructor
         function obj = AbstractModel()
             obj.version = qMRLabVer();
             obj.ModelName = class(obj);
         end
-        
+
         function saveObj(obj, suffix)
             if ~exist('suffix','var'), suffix = class(obj); end
             try
                 objStruct = objProps2struct(obj);
-                
+
                 save([regexprep(regexprep(suffix,'.qmrlab.mat','','ignorecase'),'.mat','','ignorecase') '.qmrlab.mat'], '-struct', 'objStruct');
-                
+
                 if moxunit_util_platform_is_octave
-          
+
                  save('-mat7-binary', [regexprep(regexprep(suffix,'.qmrlab.mat','','ignorecase'),'.mat','','ignorecase') '.qmrlab.mat'], '-struct' ,'objStruct');
                 end
-                
-                
+
+
             catch ME
                 error(ME.identifier, [class(obj) ':' ME.message])
             end
         end
-        
+
         function obj = loadObj(obj, fileName)
             try
                 if isstruct(fileName) % load a structure
@@ -46,16 +46,29 @@ classdef (Abstract) AbstractModel
                 end
                 % parse structure to object
                 obj = qMRpatch(obj,loadedStruct,loadedStruct.version);
-                
+
             catch ME
                 error(ME.identifier, [class(obj) ':' ME.message])
             end
         end
-        
+
         % Do some error checking
         function sanityCheck(obj,data)
-           % check if all necessary inputs are present
-           
+           mode = struct('WindowStyle','modal','Interpreter','tex');
+           % check if all necessary inputs are present and in correct order
+           MRIinputs = fieldnames(data);
+           %if data is empty
+           %if required number of inputs
+           for i=1:length(obj.reqInputs)
+               if obj.reqInputs(i) %if it's required input
+                   if(~any(strcmp(obj.MRIinputs{i},MRIinputs')))
+                       txt=strcat('Cannot find required input called ',cellstr(obj.MRIinputs{i}),'. Your input is ',cellstr(MRIinputs{i}));
+                       h = errordlg(txt,'Input Error', mode);
+                       uiwait(h)
+                       error('The input data is incorrect')
+                   end                      
+               end
+           end
            % check if all input data is sampled the same way
            MRIinputs = fieldnames(data);
            MRIinputs(structfun(@isempty,data))=[];
@@ -63,25 +76,25 @@ classdef (Abstract) AbstractModel
            qData = double(data.(MRIinputs{1}));
            x = 1; y = 1; z = 1;
            [x,y,z,nT] = size(qData);
-           
-           % check if protocol matched data
+
+           % check if protocol matches data
         end
     end
-    
 
-    
+
+
     methods(Access = protected)
         function obj = qMRpatch(obj,loadedStruct, version)
             objStruct = objProps2struct(obj);
             objectProperties = fieldnames(objStruct);
             %Loop through all object properties
             for propIndex = 1:length(objectProperties)
-                
+
                 % Assign object property value to identically named struct field.
                 obj.(objectProperties{propIndex}) = loadedStruct.(objectProperties{propIndex});
             end
-            
+
         end
     end
-    
+
 end
