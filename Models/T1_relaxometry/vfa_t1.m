d65a0543dd5f5f20361eab5fb61fb1c7d98a1409
Ilana LEPPERT <ilana.leppert@mcgill.ca>
2018-02-14 17:07:16 -0500

    add reqInputs property

--- Models/T1_relaxometry/vfa_t1.m
+++ Models/T1_relaxometry/vfa_t1.m
@@ -53,6 +53,7 @@ end
         MRIinputs = {'VFAData','B1map','Mask'};
         xnames = {'M0','T1'};
         voxelwise = 1;
+        reqInputs = [1 0 0];
         
         % Protocol
         Prot  = struct('VFAData',struct('Format',{{'FlipAngle' 'TR'}},...
