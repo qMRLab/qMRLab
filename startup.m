422903b6abf9f87c65ee9b2bd28e83c1cb3ddc7c
agahkarakuzu <agahkarakuzu@gmail.com>
2018-03-08 18:40:39 -0500

    - Octave cannot load __collect_constraints__ * Downgrade optim from 1.5.2 to 1.5.1

--- startup.m
+++ startup.m
@@ -4,7 +4,7 @@ addpath(genpath(pwd))
 
 if moxunit_util_platform_is_octave
     more off;
-    installlist = {'struct-1.0.14.tar.gz','optim-1.5.2.tar.gz','io-2.4.10.tar.gz','statistics-1.3.0.tar.gz','image-2.6.1.tar.gz'};
+    installlist = {'struct-1.0.14.tar.gz','optim-1.5.1.tar.gz','io-2.4.10.tar.gz','statistics-1.3.0.tar.gz','image-2.6.1.tar.gz'};
     loadlist = {'struct','optim','io','statistics','image'};
     for ii=1:length(installlist)
         try
@@ -28,7 +28,12 @@ if moxunit_util_platform_is_octave
             end
         end
     end
-    
+disp('--------------------------------------------')    
+path
+disp('--------------------------------------------')
+unix('ls "/home/travis/octave/optim-1.5.2/private"')
+disp('--------------------------------------------')  
+
 end
 
 
