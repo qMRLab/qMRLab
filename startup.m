9c1a4d1f05ea442ee50b35f2f5d3b5decd03e5de
agahkarakuzu <agahkarakuzu@gmail.com>
2018-03-08 18:16:11 -0500

    Octave package load issues

--- startup.m
+++ startup.m
@@ -3,14 +3,15 @@ addpath(genpath(pwd))
 % install octave package
 
 if moxunit_util_platform_is_octave
+    more off;
     installlist = {'struct-1.0.14.tar.gz','optim-1.5.2.tar.gz','io-2.4.10.tar.gz','statistics-1.3.0.tar.gz','image-2.6.1.tar.gz'};
     loadlist = {'struct','optim','io','statistics','image'};
     for ii=1:length(installlist)
         try
             disp(['Installing --> ' installlist{ii}])
             pkg('install',installlist{ii})
-            disp(['Loading -->' installlist{ii}])
-            pkg('load',loadllist{ii})
+            disp(['Loading -->' loadlist{ii}])
+            eval(['pkg load ' loadllist{ii}])
         catch
             errorcount = 1;
             while errorcount % try to install 30 times (Travis)
@@ -27,6 +28,7 @@ if moxunit_util_platform_is_octave
             end
         end
     end
+    
 end
 
 
