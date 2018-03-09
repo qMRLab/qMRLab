79e1bf28bd9d957ff77eff22aa84674852e5844b
Ilana Leppert <ilanaleppert@Ilanas-MacBook-Pro.local>
2018-03-09 10:16:24 -0500

    change colormap back to jet

--- Common/qMRshowOutput.m
+++ Common/qMRshowOutput.m
@@ -10,9 +10,9 @@ hmap = figure();
 if length(size(outputIm))>2
     sz = size(outputIm);
     szz = round(sz(3)/2);
-    imagesc(imrotate(outputIm(:,:,szz),90)); colormap('parula');  title(FitResults.fields{1});
+    imagesc(imrotate(outputIm(:,:,szz),90)); colormap('jet');  title(FitResults.fields{1});
 else
-    imagesc(imrotate(outputIm,90)); colormap('parula');  title(FitResults.fields{1});
+    imagesc(imrotate(outputIm,90)); colormap('jet');  title(FitResults.fields{1});
 end
 climm = prctile(outputIm(outputIm~=0),20);
 climM = prctile(outputIm(outputIm~=0),80);
