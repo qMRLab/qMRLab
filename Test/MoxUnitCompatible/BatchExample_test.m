abc3b671c68d0c43032656e3e916c57f55d5dcee
agahkarakuzu <agahkarakuzu@gmail.com>
2018-03-13 15:37:01 -0400

    [Travis] No longer caching OSF [Travis] Divide and conquer SimTests

--- Test/MoxUnitCompatible/BatchExample_test.m
+++ Test/MoxUnitCompatible/BatchExample_test.m
@@ -11,18 +11,9 @@ setenv('ISTRAVIS','1') % go faster! Fit only 2 voxels in FitData.m
 function test_batch
 curdir = pwd;
 
-% ======================================== TRAVIS CACHE SELECTION
 
-% Set <<cahceState>> to:
-
-%  <false> if cache (for OSF data) is cleared or to be updated
-%  <true> if the same cache (for OSF data) is still in use
-
-cacheState = false;
-% -----------------------------------------------------
-
-% This directory will be created on Travis server. See .travis.yml
 tmpDir = '/home/travis/build/neuropoly/qMRLab/osfData';
+mkdir(tmpDir);
 cd(tmpDir)
 
 Modellist = list_models';
@@ -30,32 +21,25 @@ for iModel = 1:length(Modellist)
     disp('===============================================================')
     disp(['Testing: ' Modellist{iModel} ' BATCH...'])
     disp('===============================================================')
-
+    
     
     eval(['Model = ' Modellist{iModel}]);
     
-  
-    if cacheState
-        % Navigate to the batch example folders 
-        cd([tmpDir filesep Modellist{iModel} '_demo']);
-    end
     
     
     
-    if ~cacheState
-        % Generate batch w/ downloading data
-        qMRgenBatch(Model,pwd)
-    elseif cacheState
-        % Generate batch w/o downloading data
-        qMRgenBatch(Model,pwd,0)
-    end
     
- 
+    
+    
+    qMRgenBatch(Model,pwd)
+    
+    
+    
     
     % Test if any dataset exist
     isdata = true;
-    try 
-        Model.onlineData_url; 
+    try
+        Model.onlineData_url;
     catch
         isdata = false;
     end
@@ -68,7 +52,7 @@ for iModel = 1:length(Modellist)
     end
     close all
     cd ..
-   
+    
 end
 cd(curdir)
 
