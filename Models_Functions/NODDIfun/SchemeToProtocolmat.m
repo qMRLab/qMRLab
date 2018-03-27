07973f0cceb8da138b0e1218a70f277b76735f81
Tanguy DUVAL <duvaltanguy@gmail.com>
2018-03-27 19:19:30 +0200

    normalize G with bvec

--- Models_Functions/NODDIfun/SchemeToProtocolmat.m
+++ Models_Functions/NODDIfun/SchemeToProtocolmat.m
@@ -13,7 +13,7 @@ Prot = Prot';
 % Create the protocol
 protocol.pulseseq  = 'PGSE';
 protocol.grad_dirs = Prot(1:3,:)';
-protocol.G         = Prot(4,:);
+protocol.G         = Prot(4,:).*sqrt(sum(Prot(1:3,:).^2,1));
 protocol.delta     = Prot(5,:);
 protocol.smalldel  = Prot(6,:);
 protocol.TE        = Prot(7,:);
