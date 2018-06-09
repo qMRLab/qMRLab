f0adc58fd740209263b7a658e582234505ed53ab
Mathieu Boudreau <emb6150@gmail.com>
2018-06-09 14:30:56 -0230

    Fix image plot proportions

--- src/Models_Functions/QSM/plot_axialSagittalCoronal.m
+++ src/Models_Functions/QSM/plot_axialSagittalCoronal.m
@@ -4,9 +4,9 @@ function [  ] = plot_axialSagittalCoronal( vol, fig_no, scale_fig, title_fig )
 
 im_size = round(size(vol)/2);
 
-figure(fig_no), subplot(1,3,1), imagesc( vol(:,:,im_size(3)), scale_fig  ), axis square off, colormap gray
-figure(fig_no), subplot(1,3,3), imagesc( imrotate(squeeze(vol(:,im_size(2),:)),90), scale_fig  ), axis square off, colormap gray
-figure(fig_no), subplot(1,3,2), imagesc( imrotate(squeeze(vol(im_size(1),:,:)),90), scale_fig  ), axis square off, colormap gray
+figure(fig_no), subplot(1,3,1), imagesc( vol(:,:,im_size(3)), scale_fig  ), axis square off, colormap gray, axis image
+figure(fig_no), subplot(1,3,3), imagesc( imrotate(squeeze(vol(:,im_size(2),:)),90), scale_fig  ), axis square off, colormap gray, axis image
+figure(fig_no), subplot(1,3,2), imagesc( imrotate(squeeze(vol(im_size(1),:,:)),90), scale_fig  ), axis square off, colormap gray, axis image
 
 if nargin == 4
     title(title_fig)
