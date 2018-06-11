842c76beb076b2aee6ec778d60452bb229d027e8
Tanguy DUVAL <duvaltanguy@gmail.com>
2018-06-12 01:00:27 +0200

    [DOC] correct mistakes, improve experience

--- src/Common/qMRloadObj.m
+++ src/Common/qMRloadObj.m
@@ -1,4 +1,10 @@
 function Model = qMRloadObj(filename)
+% qMRloadObj    Load a qMR Model file (.qmr file)
+%
+% Example
+%   Model = qMRloadObj('mwf.qmr');
+%   qMRusage(Model)
+if nargin==0, help('qMRloadObj'); return; end
 if ~isstruct(filename) % load directly structure
     S = load(filename);
 else
