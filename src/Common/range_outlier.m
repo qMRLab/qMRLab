673ffa8b59d0458c82404106f88786a54a8d427f
Tanguy DUVAL <duvaltanguy@gmail.com>
2018-08-16 21:37:45 +0200

    solves range_outlier bug

--- src/Common/range_outlier.m
+++ src/Common/range_outlier.m
@@ -1,4 +1,5 @@
-function [xmin, xmax] = range_outlier(x)
+function [xmin, xmax] = range_outlier(x,qt)
+if nargin<2, qt=5; end
 % detect outliers based on 5 times the Interquartile Range
 x=x(:); x(isinf(x))=[]; x(isnan(x))=[]; 
 
@@ -6,17 +7,32 @@ x=x(:); x(isinf(x))=[]; x(isnan(x))=[];
 y = sort(x);
 
 % compute 25th percentile (first quartile)
-Q(1) = median(y(y<median(y)));
+Q1L = y<median(y);
+if ~any(Q1L)
+  Q(1)=min(y);
+  else
+  Q(1) = median(y(Q1L)); 
+end
 
 % compute 50th percentile (second quartile)
 Q(2) = median(y);
 
 % compute 75th percentile (third quartile)
-Q(3) = median(y(y>median(y)));
+Q3L = y>median(y);
+if ~any(Q3L)
+  Q(3)=max(y);
+  else
+  Q(3) = median(y(Q3L));
+
+end
 
 % compute Interquartile Range (IQR)
 IQR = Q(3)-Q(1);
 
 % determine extreme Q1 outliers (e.g., x < Q1 - 3*IQR)
-xmin = max(min(y),Q(1)-5*IQR);
-xmax = min(max(y),Q(3)+5*IQR);
+xmin = max(min(y),Q(1)-qt*IQR);
+xmax = min(max(y),Q(3)+qt*IQR);
+
+if nargout<2
+    xmin = [xmin xmax];
+end
