b7afa0b7d218d89170c72da5f276a4fe14e80a8b
Agah <agahkarakuzu@gmail.com>
2018-09-19 13:01:52 -0400

    Add new link
    -- getLink contains both links now
    -- Test data is under PartialDatasets/qsm_sb.zip on OSF

--- src/Models_Functions/QSM/getLink.m
+++ src/Models_Functions/QSM/getLink.m
@@ -3,7 +3,7 @@ function  link = getLink
   if ~isempty(getenv('ISTRAVIS')) && str2double(getenv('ISTRAVIS'))
     link = 'https://osf.io/549ke/download/';
   else
-    link  = 'newlink';
+    link  = 'https://osf.io/9d8kz/download/';
   end
-  
+
 end
