800fd6f584a6585d8b6c286be75a6b870a2f32bd
Mathieu Boudreau <emb6150@gmail.com>
2018-10-15 12:59:43 -0230

    [REF] Quiet down two statements that are incorrectly too verbose

--- src/Common/qMRinfo.m
+++ src/Common/qMRinfo.m
@@ -15,5 +15,5 @@ if ~moxunit_util_platform_is_octave
     [varargout{1:nargout}] = help(varargin{:});
 else
     varargout{1} = 'Sorry, not implemented for Octave yet. Please contribute on github.com/neuropoly/qMRLab';
-	disp(varargout{1})
+	% disp(varargout{1}) Feature disabled temporarily. See issue #275, this gets printed incorrectly every time when FitData is used in Octave
 end
