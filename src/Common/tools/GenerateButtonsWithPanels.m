a2f72cb746ab391b7b98dcf0fcfb7ac9fc591eef
Mathieu Boudreau <emb6150@gmail.com>
2018-10-15 16:29:19 -0230

    [BUG] Fix index for shortened joker

--- src/Common/tools/GenerateButtonsWithPanels.m
+++ src/Common/tools/GenerateButtonsWithPanels.m
@@ -261,7 +261,7 @@ for ii = 1:N
     val = opts{2*ii};
 
     % special case: disable if ### in option name
-    if strcmp(opts{2*ii-1}(1:3),'##')
+    if strcmp(opts{2*ii-1}(1:2),'##')
 
        opts{2*ii-1} = opts{2*ii-1}(4:end);
        disable=true;
