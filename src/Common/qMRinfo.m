cd6df176b7d824a3b3c884a77148167ea8bdd452
Tanguy DUVAL <duvaltanguy@gmail.com>
2018-10-18 09:51:18 +0200

    cannot call help for standalone. temporary make it empty

--- src/Common/qMRinfo.m
+++ src/Common/qMRinfo.m
@@ -12,7 +12,11 @@
 function varargout = qMRinfo(varargin)
 if ~moxunit_util_platform_is_octave
     if nargin==0, varargin{1}='qMRinfo'; end
-    [varargout{1:nargout}] = help(varargin{:});
+    if isdeployed
+        varargout={''};
+    else
+    [varargout{1:nargout}] = help(varargin{:});        
+    end
 else
     varargout{1} = 'Sorry, not implemented for Octave yet. Please contribute on github.com/neuropoly/qMRLab';
 	disp(varargout{1})
