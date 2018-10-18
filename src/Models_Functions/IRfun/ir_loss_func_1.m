4596447abb65c0b852745bbefa3470c1614d2ce0
Mathieu Boudreau <emb6150@gmail.com>
2018-10-18 13:55:58 -0230

    [HOTFIX] Remove residual disp() call from a debugging session

--- src/Models_Functions/IRfun/ir_loss_func_1.m
+++ src/Models_Functions/IRfun/ir_loss_func_1.m
@@ -8,6 +8,6 @@ params.constant = x(1);
 params.T1 = x(2);
 params.EXC_FA = x(3);
 params.INV_FA = x(4);
-disp(params)
+
 lossVal = inversion_recovery.analytical_solution(params, 'GRE-IR', 1) - data;
 end
