fc6887935af2eff4fd11765794e27400109c7a59
Ilana LEPPERT <ilana.leppert@mcgill.ca>
2018-12-17 16:07:22 -0500

    inclusion of filterclass and interaction with its functions

--- src/Models/FieldMaps/b1_dam.m
+++ src/Models/FieldMaps/b1_dam.m
@@ -2,20 +2,33 @@ classdef b1_dam < AbstractModel & FilterClass
 % b1_dam map:  Double-Angle Method for B1+ mapping
 %
 % Assumptions:
-%   Compute a B1map using 2 SPGR images with 2 different flip angles (60, 120deg)
+%   Compute a B1map using 2 SPGR images with 2 different flip angles (alpha, 2xalpha)
+%   Smoothing can be done with different filters and optional size
+%   Spurious B1 values and those outside the mask (optional) are set to a constant before smoothing
 %
 % Inputs:
 %   SFalpha            SPGR data at a flip angle of Alpha degree
 %   SF2alpha           SPGR data at a flip angle of AlphaX2 degree
+%   (Mask)             Binary mask to exclude non-brain voxels (better when smoothing)
 %
 % Outputs:
-%	B1map           Excitation (B1+) field map
+%	B1map_raw          Excitation (B1+) field map
+%   B1map              Smoothed B1+ field map using Gaussian or Median filter
+%   Spurious           Map of datapoints that were set to 1 prior to smoothing
 %
 % Protocol:
 %	NONE
 %
-% Options
-%   NONE
+% Options:
+%   Smoothing Filter
+%     Type                 Type of filter
+%                              - gaussian
+%                              - median
+%     Dimension            In which dimensions to apply the filter
+%                               -2D
+%                               -3D
+%     size(x,y,z)          Extent of filter in # of voxels
+%                               For gaussian, it's FWHM
 %
 % Example of command line usage:
 %   Model = b1_dam;% Create class from model
@@ -44,7 +57,7 @@ properties (Hidden=true)
 end
 
     properties
-        MRIinputs = {'SFalpha','SF2alpha'};
+        MRIinputs = {'SFalpha','SF2alpha','Mask'};
         xnames = {};
         voxelwise = 0; % 0, if the analysis is done matricially
                        % 1, if the analysis is done voxel per voxel
@@ -52,14 +65,10 @@ end
         % Protocol
         Prot = struct('Alpha',struct('Format',{'FlipAngle'},'Mat',60));
 
-        % Model options
-        buttons ={'PANEL','Smoothing filter',5,...
-            'Type',{'gaussian','median'},...
-            'Dimension',{'3D','2D'},...
-            'FWHM x',3,...
-            'FWHM y',3,...
-            'FWHM z',3};
-        options = struct(); % structure filled by the buttons. Leave empty in the code
+        % Inherit these from public properties of FilterClass 
+%         % Model options
+%         buttons ={};
+%         options = struct(); % structure filled by the buttons. Leave empty in the code
 
     end
 
@@ -72,34 +81,20 @@ end
         function obj = b1_dam
             obj.options = button2opts(obj.buttons);
         end
-        function obj = UpdateFields(obj)
-          
-            % Disable/enable some options --> Add ### to the button
-            % Name you want to disable
-            disablelist = {'FWHM z'};
-            switch  obj.options.Smoothingfilter_Dimension
-                case {'2D'}
-                    disable = true; 
-                    obj.options.Smoothingfilter_FWHMz=0;
-                otherwise
-                    disable = false;
-            end
-            for ll = 1:length(disablelist)
-                indtodisable = find(strcmp(obj.buttons,disablelist{ll}) | strcmp(obj.buttons,['##' disablelist{ll}]));
-                if disable(ll)
-                    obj.buttons{indtodisable} = ['##' disablelist{ll}];
-                else
-                    obj.buttons{indtodisable} = [disablelist{ll}];
-                end
-            end
-        end
+       
         
         function FitResult = fit(obj,data)
             FitResult.B1map_raw = abs(acos(data.SF2alpha./(2*data.SFalpha))./(deg2rad(obj.Prot.Alpha.Mat)));
-            switch obj.options.Smoothingfilter_Type
-                case {'gaussian'}
-                    FitResult.B1map=obj.gaussFilt(FitResult.B1map,[obj.options.Smoothingfilter_FWHMx,obj.options.Smoothingfilter_FWHMy,obj.options.Smoothingfilter_FWHMz]); %smoothed
+            %remove 'spurious' points to reduce edge effects
+            if isfield(data,'Mask') && ~isempty(data.Mask)
+                FitResult.B1map_raw = data.Mask .* FitResult.B1map_raw;
             end
+            FitResult.Spurious = double(FitResult.B1map_raw<0.5);
+            B1map_nospur = FitResult.B1map_raw;
+            B1map_nospur(B1map_nospur<0.6)=1; %set 'spurious' values to 1
+            
+            % call the superclass (FilterClass) fit function
+            FitResult.B1map=struct2array(fit@FilterClass(obj,B1map_nospur,[obj.options.Smoothingfilter_sizex,obj.options.Smoothingfilter_sizey,obj.options.Smoothingfilter_sizez]));
         end
 
     end
