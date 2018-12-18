2ac274508915788f7a1db56d6836b23f33a693c4
Tanguy Duval <duvaltanguy@gmail.com>
2018-12-18 10:55:13 +0100

    use imtool3D range_outlier file

--- External/imtool3D_td/External/range_outlier.m
+++ External/imtool3D_td/External/range_outlier.m
@@ -1,25 +1,29 @@
 function [xmin, xmax] = range_outlier(x,qt)
-if nargin<2, qt=5; end
+if nargin<2, qt=1; end
+
 % detect outliers based on 5 times the Interquartile Range
 x=x(:); x(isinf(x))=[]; x(isnan(x))=[]; 
 
+% STEP 1 - rank the data
+y = sort(x);
+
 % compute 25th percentile (first quartile)
-Q1L = x<median(x);
+Q1L = y<median(y);
 if ~any(Q1L)
-  Q(1)=min(x);
+  Q(1)=min(y);
   else
-  Q(1) = median(x(Q1L)); 
+  Q(1) = median(y(Q1L)); 
 end
 
 % compute 50th percentile (second quartile)
-Q(2) = median(x);
+Q(2) = median(y);
 
 % compute 75th percentile (third quartile)
-Q3L = x>median(x);
+Q3L = y>median(y);
 if ~any(Q3L)
-  Q(3)=max(x);
+  Q(3)=max(y);
   else
-  Q(3) = median(x(Q3L));
+  Q(3) = median(y(Q3L));
 
 end
 
@@ -27,8 +31,9 @@ end
 IQR = Q(3)-Q(1);
 
 % determine extreme Q1 outliers (e.g., x < Q1 - 3*IQR)
-xmin = max(min(x),Q(1)-qt*IQR);
-xmax = min(max(x),Q(3)+qt*IQR);
+
+xmin = max(min(y),Q(1)-qt*IQR);
+xmax = min(max(y),Q(3)+qt*IQR);
 
 if nargout<2
     xmin = [xmin xmax];
