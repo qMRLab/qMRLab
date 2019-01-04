ba34f4a5bef899bcccedd19b7c0c0f80f69a44bb
agahkarakuzu <agahkarakuzu@gmail.com>
2019-01-04 15:35:27 -0500

    Perturb VFA

--- src/Models_Functions/MTV/mtv_compute_m0_t1.m
+++ src/Models_Functions/MTV/mtv_compute_m0_t1.m
@@ -56,16 +56,16 @@ for vox=1:dims(1)*dims(2)*dims(3)
             % Flip Angle Correction - Liberman, et al.
             y = squeeze(squeeze(data(ii,jj,kk, :)))./sin(flipAngles/180*pi*b1Map(vox))';
             x = squeeze(squeeze(data(ii,jj,kk, :)))./tan(flipAngles/180*pi*b1Map(vox))';
-            
+
             % fit data
             param = polyfit(x,y,1);
 %             fitresult = LinearFit(x, y, verbose);
 %             param = coeffvalues(fitresult); % slope and intercept of the fitting
             param(isnan(param))=0;
-            
+
 %             ci = confint(fitresult,0.682); % confidence interval of the fitting (returns the slope and intercept of the lines framing the fit) -- corresponding to 2*sigma
 %             ci(isnan(ci)) = 0;
-%            
+%
             % compute PD and T1
             [~,T1(vox)]=getT1(param,TR);
         else
@@ -74,18 +74,18 @@ for vox=1:dims(1)*dims(2)*dims(3)
         end
         %% Get M0
         [FA,iFA]=min(flipAngles);
-        
+
         M0(vox)=getM0fromT1(T1(vox),TR(1),data(ii,jj,kk,iFA),FA*b1Map(vox));
         % add length of the confidence interval at 68.2% in the fourth
         % dimension
-        
+
         if ~fixT1
 %             [~,t1_min]=getT1(ci(1,:),TR);
 %             [~,t1_max]=getT1(ci(2,:),TR);
 %             M02(vox)=abs(getM0fromT1(t1_max,TR,data(ii,jj,kk,ialpha),alpha)-getM0fromT1(t1_min,TR,data(ii,jj,kk,ialpha),alpha));
 %             T12(vox)=abs(t1_max-t1_min);
         end
-        
+
     end
 end
 if ~fixT1
@@ -122,7 +122,7 @@ ft = fittype( 'poly1' );
 
 % Plot fit with data.
 if verbose && max(xData~=0) && max(yData~=0)
-    
+
     figure(100)
     h = plot( fitresult, xData, yData,'+');
     set(h,'MarkerSize',30)
@@ -147,7 +147,7 @@ if a>0
 else  % due to noise or bad fitting
     T1 = 0.000000000000001;
 end
-M0 = b/(1-exp(-TR/T1));
+M0 = b/(1+exp(-TR/T1));
 
 
 function M0=getM0fromT1(T1,TR,S,FA)
