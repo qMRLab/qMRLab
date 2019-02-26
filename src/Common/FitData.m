787a01abb0c9da494542ecb2e070e2bbaf876c8a
Mathieu Boudreau <emb6150@gmail.com>
2019-02-26 20:03:30 -0330

    Reverted change in fitting voxel display for command line or Octave calls

--- src/Common/FitData.m
+++ src/Common/FitData.m
@@ -94,7 +94,7 @@ if Model.voxelwise % process voxelwise
         setappdata(h,'canceling',0)
     end
 
-    if (isempty(h)), fprintf('Fitting %d voxels.\n',numVox); end
+    if (isempty(h)), j_progress('Fitting voxel ',l); end
     for ii = 1:numVox
         vox = Voxels(ii);
 
