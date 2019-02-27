62b163410dd639c1ce1b6f48f07aa1e754c6721e
Ilana LEPPERT <ilana.leppert@mcgill.ca>
2019-02-27 16:22:22 -0500

    for polynomial, we want to be able to pass the mask, don't want to fit in noise

--- src/Common/FilterClass.m
+++ src/Common/FilterClass.m
@@ -23,7 +23,7 @@ classdef (Abstract) FilterClass
             'size x',3,...
             'size y',3,...
             'size z',3,...
-            'order',12};
+            'order',5};
         options = struct(); % structure filled by the buttons. Leave empty in the code
     end
     
@@ -51,7 +51,7 @@ classdef (Abstract) FilterClass
             % 3D cases
             if strcmp(obj.options.Smoothingfilter_Type,{'polynomial'})
                 disable = [true true true false];
-%                 obj.options.Smoothingfilter_Dimension={'2D'};
+    
             end
             
             for ll = 1:length(disablelist)
@@ -67,11 +67,11 @@ classdef (Abstract) FilterClass
         function FitResult = fit(obj,data,size)
             switch obj.options.Smoothingfilter_Type
                 case {'gaussian'}
-                    FitResult.Filtered=obj.gaussFilt(data,size); %smoothed
+                    FitResult.Filtered=obj.gaussFilt(data.Raw,size); %smoothed
                 case {'median'}
-                    FitResult.Filtered=obj.medianFilt(data,size); %smoothed
+                    FitResult.Filtered=obj.medianFilt(data.Raw,size); %smoothed
                 case {'spline'}
-                    FitResult.Filtered=obj.splineFilt(data,size); %smoothed
+                    FitResult.Filtered=obj.splineFilt(data.Raw,size); %smoothed
                 case {'polynomial'}
                     FitResult.Filtered=obj.polyFilt(data,obj.options.Smoothingfilter_order); %smoothed
             end
@@ -81,7 +81,7 @@ classdef (Abstract) FilterClass
             % Apply a Gaussian filter (2D or 3D)
             % fwhm is a 2 or 3-element vector of positive numbers
             sigmaPixels = fwhm2sigma(fwhm); %Full width half max of desired gaussian kernel (in #voxels) converted to sigma of Gaussian
-            if(sigmaPixels(3)==0 || strcmp(obj.options.Smoothingfilter_Dimension,'{2D}')) %for the 2D case
+            if(sigmaPixels(3)==0 || strcmp(obj.options.Smoothingfilter_Dimension,'2D')) %for the 2D case
                 filtered = imgaussfilt(data,sigmaPixels(1:2));
             else
                 filtered = imgaussfilt3(data,sigmaPixels);
@@ -92,7 +92,7 @@ classdef (Abstract) FilterClass
         function filtered =medianFilt(obj,data,s)
             % Apply a median filter (2D or 3D)
             % s is a 3-element vector of positive numbers (voxels)
-            if(s(3)==0 || strcmp(obj.options.Smoothingfilter_Dimension,'{2D}')) %for the 2D case, if the volume is 3D, have to do each slice separately
+            if(s(3)==0 || strcmp(obj.options.Smoothingfilter_Dimension,'2D')) %for the 2D case, if the volume is 3D, have to do each slice separately
                 if(ndims(data)==2)
                     filtered = medfilt2(data,s(1:2));
                 else
@@ -110,9 +110,9 @@ classdef (Abstract) FilterClass
         function filtered =splineFilt(obj,data,S)
             % Apply a spline filter (2D or 3D)
             s=S(1); %just 1 values for the smoothness
-            if(strcmp(obj.options.Smoothingfilter_Dimension,'{2D}')) %if want 2D smoothing
+            if(strcmp(obj.options.Smoothingfilter_Dimension,'2D')) %if want 2D smoothing
                 if(ndims(data)==2) %if a 2D volume
-                    filtered = smoothn(data,s);
+                    filtered = smoothn(dataw,s);
                 else %if a 3D volume, do each slice separately
                     filtered=zeros(size(data));
                     for i=1:size(data,3)
@@ -126,22 +126,36 @@ classdef (Abstract) FilterClass
             %filtered = smoothn(data,'robust');            
         end
         
-        %Polynomial filter
+        %% Polynomial filter
+        % for the fitting to work better, a mask should be provided, it's
+        % one of the rare methods that needs to know what the mask was and
+        % only fir in there
         function filtered=polyFilt(obj,data,order)
             % Apply a polynomial fit of the specified order (in 2D)
-            if(ndims(data)==2) %if a 2D volume
-                filtered = poly_fit(data,order);
-            % if a 3D volume, use 2D or 3D fit according to the selection of dimension:
-            % 1. Perform each slice separately in 2D poly fit; OR
-            % 2. Perform 3D poly fit
-            else
-                filtered=zeros(size(data));
-                if strcmp(obj.options.Smoothingfilter_Dimension,'{2D}')
-                    for i = 1:size(data,3)
-                        filtered(:,:,i) = poly_fit(data(:,:,i),order);
-                    end
+            if(ndims(data.Raw)==2) %if a 2D volume
+                if isfield(data,'Mask') && (~isempty(data.Mask))
+                    filtered = poly_fit(data.Raw,order,data.Mask);
                 else
-                    filtered = polyfit_3D(data(:,:,i),order);
+                    filtered = poly_fit(data.Raw,order);
+                end
+                % if a 3D volume, use 2D or 3D fit according to the selection of dimension:
+                % 1. Perform each slice separately in 2D poly fit; OR
+                % 2. Perform 3D poly fit
+            else
+                filtered=zeros(size(data.Raw));
+                if strcmp(obj.options.Smoothingfilter_Dimension,'2D')
+                    for i = 1:size(data.Raw,3)
+                        if isfield(data,'Mask') && (~isempty(data.Mask))
+                            filtered(:,:,i) = poly_fit(data.Raw(:,:,i),order,data.Mask(:,:,i));
+                        else
+                            filtered(:,:,i) = poly_fit(data.Raw(:,:,i),order);
+                        end
+                    end
+                    
+                elseif isfield(data,'Mask') && (~isempty(data.Mask))
+                    filtered = polyfit_3D(data.Raw,order,data.Mask);
+                else
+                    filtered = polyfit_3D(data.Raw,order);
                 end
             end
         end
