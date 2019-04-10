381622ce6c1314b54351295677588c231ba65324
agahkarakuzu <agahkarakuzu@gmail.com>
2019-04-09 22:45:22 -0400

    Set threshold at 5%

--- src/Models_Functions/QSM/getLink.m
+++ src/Models_Functions/QSM/getLink.m
@@ -1,14 +1,13 @@
 function  link = getLink(modelName)
   
-  TRAVIS = 0; 
-  if ~isempty(getenv('ISTRAVIS')) && str2double(getenv('ISTRAVIS'))...
-     && isempty(getenv('ISDOC')); TRAVIS = 1; end 
+  if isempty(getenv('ISTRAVIS')) || ~str2double(getenv('ISTRAVIS')), ISTRAVIS=false; else ISTRAVIS=true; end
+  if isempty(getenv('ISDOC')) || ~str2double(getenv('ISDOC')), ISDOC=false; else ISDOC=true; end
 
   switch modelName
     
   case 'qsm_sb'  
   
-    if TRAVIS
+    if ISTRAVIS
       link = 'https://osf.io/549ke/download?version=4'; % Partial dataset
     else
       link  = 'https://osf.io/9d8kz/download?version=1'; % Full dataset
@@ -16,7 +15,7 @@ function  link = getLink(modelName)
   
   case 'charmed'  
 
-    if TRAVIS
+    if ISTRAVIS
       link = 'https://osf.io/bdxa6/download?version=1'; % Octave output
     else
       link  = 'https://osf.io/u8n56/download?version=3'; % Matlab output
@@ -24,7 +23,7 @@ function  link = getLink(modelName)
 
   case 'qmt_bssfp'
     
-    if TRAVIS
+    if ISTRAVIS
       link = 'https://osf.io/28nhj/download?version=1'; % Octave output
     else
       link  = 'https://osf.io/r64tk/download?version=2'; % Matlab output
@@ -32,7 +31,7 @@ function  link = getLink(modelName)
 
   case 'qmt_sirfse'
     
-    if TRAVIS
+    if ISTRAVIS
       link = 'https://osf.io/v2k7q/download?version=1'; % Octave output
     else
       link  = 'https://osf.io/fk2nd/download?version=2'; % Matlab output
