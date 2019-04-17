2e55bb665436aebb189fd95aeb3b1e3cf74fa379
agahkarakuzu <agahkarakuzu@gmail.com>
2019-04-17 16:44:28 -0400

    Edit insert for indentation

--- Deploy/Documentation/insertBadge.m
+++ Deploy/Documentation/insertBadge.m
@@ -12,10 +12,17 @@ function insertBadge(rstDir)
         tmp = api.getTemplateFile([rstDir filesep Modellist{iModel} '_batch.rst']);
         nw = cell(length(tmp)+2,1);
         nw(1:2) = tmp(1:2);
+        % Skip line 3
         nw(4) = {'.. image:: https://mybinder.org/badge_logo.svg'};
         nw(5) = {[' :target: https://mybinder.org/v2/gh/qMRLab/doc_notebooks/master?filepath=' Modellist{iModel} '_notebook.ipynb']};
         nw(6:end) = tmp(4:end);
-
+        
+        % indent html block (omit tag)
+        for ii=7:length(nw)
+           nw(ii) = {sprintf('\t%s',nw{ii})}; 
+        end
+        
+        
         fileID = fopen([rstDir filesep Modellist{iModel} '_batch.rst'],'w');
         formatSpec = '%s\n';
         [nrows,~] = size(nw);
