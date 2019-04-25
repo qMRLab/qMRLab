29f750945afe713d1b4e3ce655fcb4085092636e
agahkarakuzu <agahkarakuzu@gmail.com>
2019-04-25 19:59:40 -0400

    Log in to docker.io domain

--- Deploy/Docker/MCR/build.sh
+++ Deploy/Docker/MCR/build.sh
@@ -17,7 +17,7 @@ cd $AGENT_RELEASEDIRECTORY/$RELEASE_PRIMARYARTIFACTSOURCEALIAS/Deploy/Docker/MCR
 docker build -t $USERNAME/$IMAGE:$version -t $USERNAME/$IMAGE:latest --build-arg TAG=$version .
 
 # PUSH
-docker login -u=$DOCKER_USERNAME -p=$DOCKER_PASSWORD
+docker login -u=$DOCKER_USERNAME -p=$DOCKER_PASSWORD docker.io
 docker push $USERNAME/$IMAGE:latest
 docker push $USERNAME/$IMAGE:$version
 
