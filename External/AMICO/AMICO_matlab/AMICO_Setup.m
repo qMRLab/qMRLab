f9a9297269ee482b42318d6a9295def4d1734a38
Tanguy Duval <duvaltanguy@gmail.com>
2019-09-30 14:38:47 +0200

    rm gitignore AMICO_SETUP

---
+++ External/AMICO/AMICO_matlab/AMICO_Setup.m
@@ -0,0 +1,13 @@
+%
+% Initialization for AMICO
+%
+% NB: DO NOT MODIFY THIS FILE!
+%     Make a copy of it, adapt to your paths and rename it to "AMICO_Setup.m"
+%
+
+global AMICO_code_path AMICO_data_path
+
+% Path definition: adapt these to your needs
+% ==========================================
+AMICO_code_path = fileparts(mfilename('fullpath'));
+AMICO_data_path = fullfile(AMICO_code_path,'exports');
