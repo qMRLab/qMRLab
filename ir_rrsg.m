e08b1eded30a0330e0b0692264e213f09bd0f1a1
Mathieu Boudreau <emb6150@gmail.com>
2020-02-12 21:14:17 -0330

    Update ir_rrsg.m

--- ir_rrsg.m
+++ ir_rrsg.m
@@ -4,8 +4,10 @@ load(inputFilename)
 
 % Assign data to variables needed for qMRLab module
 IRData = data;
-Mask = mask;
 
+if exist('mask')
+  Mask = mask;
+end
 
 % Format qMRLab inversion_recovery model parameters, and load them into the Model object
 Model = inversion_recovery; 
@@ -17,7 +19,10 @@ Model.options.method = dataType;
 % Format data structure so that they may be fit by the model
 data = struct();
 data.IRData= double(IRData);
-data.Mask= double(Mask);
+
+if exist('mask')
+  data.Mask= double(Mask);
+end
 
 FitResults = FitData(data,Model,0); % The '0' flag is so that no wait bar is shown.
 
