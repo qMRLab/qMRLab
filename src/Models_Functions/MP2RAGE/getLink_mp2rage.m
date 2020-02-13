95a5af3ad7c1cf277727e215b523e613602e3946
agahkarakuzu <agahkarakuzu@gmail.com>
2020-02-13 16:04:38 -0500

    mp2rage subset

---
+++ src/Models_Functions/MP2RAGE/getLink_mp2rage.m
@@ -0,0 +1,9 @@
+function  link = getLink_mp2rage
+
+  if ~isempty(getenv('ISTRAVIS')) && str2double(getenv('ISTRAVIS'))
+    link = 'https://osf.io/8x2c9/download?version=4';
+  else
+    link  = 'https://osf.io/k3shf/download?version=1';
+  end
+
+end
