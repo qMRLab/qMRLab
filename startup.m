5a9a1079111ad3d17c8579202b90467ec29e7f2c
Agah Karakuzu <agahkarakuzu@gmail.com>
2021-01-13 23:03:58 -0500

    Interesting

--- startup.m
+++ startup.m
@@ -11,7 +11,12 @@ end
 
 if ~moxunit_util_platform_is_octave % MATLAB
     % Test Optimization toolbox is installed
-    if isempty(getenv('ISAZURE')) || ~str2double(getenv('ISAZURE')), ISAZURE=false; else ISAZURE=true; end
+    if isempty(getenv('ISAZURE')) || ~str2double(getenv('ISAZURE')) 
+        ISAZURE=false; 
+    else
+        ISAZURE=true; 
+    end
+    disp(['Azure env is' getenv('ISAZURE')]);
     if ~ISAZURE
       if ~license('test', 'Optimization_Toolbox'), error('Optimization Toolbox is not installed on your system: most qMR models won''t fit. Please consider installing <a href="matlab:matlab.internal.language.introspective.showAddon(''OP'');">Optimization Toolbox</a> if you want to use qMRLab in MATLAB.'); end
       if ~license('test', 'Image_Toolbox'), warning('Image Toolbox is not installed: ROI Analysis tool not available in the GUI. Consider installing <a href="matlab:matlab.internal.language.introspective.showAddon(''IP'');">Image Processing Toolbox</a>'); end
