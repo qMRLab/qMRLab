9850175e714bea593572a03c8137956b80f62110
Agah Karakuzu <agahkarakuzu@gmail.com>
2021-02-25 16:34:24 -0500

    Fix

--- Deploy/Documentation/NotebookGen/PushNotebooks.sh
+++ Deploy/Documentation/NotebookGen/PushNotebooks.sh
@@ -1,6 +1,8 @@
- # If to execute manually, pass qMRLab's root path  as .../qMRLab
-# If Azure release, use full path. 
-if [[-z "${AGENT_RELEASEDIRECTORY}"]]; then
+# This one is intended only for Azure. 
+# Do not push tokens if not Azure secrets, otherwise 
+# going to be exposed!!!!!
+
+if [ -z "${AGENT_RELEASEDIRECTORY}" ]; then
     qMRdir=$AGENT_RELEASEDIRECTORY/$RELEASE_PRIMARYARTIFACTSOURCEALIAS
     version=`cat $qMRdir/version.txt`
 else # Otherwise use relative path. 
@@ -36,8 +38,9 @@ mv -v $nbSub/* $nbSub/doc_notebooks
 git add .
 git commit -m "For $version on $NOW"
 git tag -a "$version" -m "version $version"
-git push https://$1@github.com/qMRLab/doc_notebooks.git -f
-git push https://$1@github.com/qMRLab/doc_notebooks.git --tags
-
+if [ -z "${AGENT_RELEASEDIRECTORY}" ]; then
+    git push https://$1@github.com/qMRLab/doc_notebooks.git -f
+    git push https://$1@github.com/qMRLab/doc_notebooks.git --tags
+fi
 # Remove temporary files 
 rm -rf /tmp/qMRLabJNB
