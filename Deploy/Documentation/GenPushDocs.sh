1b3821a1c6ad1f418fcbc31fc67f663a43f0545b
Agah Karakuzu <agahkarakuzu@gmail.com>
2021-02-25 23:47:50 -0500

    try

--- Deploy/Documentation/GenPushDocs.sh
+++ Deploy/Documentation/GenPushDocs.sh
@@ -37,7 +37,7 @@ branchNameQMRLAB=${{ Build.SourceBranchName }}
 # qMRLab branch name.
 git checkout -b $branchNameQMRLAB
 #Generate new documentation sources in this new branch
-matlab -nodisplay -nosplash -r "cd('$qMRdir'); startup; GenerateDocumentation('$docDir'); exit;"
+matlab -nosplash -r "cd('$qMRdir'); startup; GenerateDocumentation('$docDir'); exit;"
 
 git config user.email "$GITHUB_MAIL"
 git config user.name "$GITHUB_NAME"
