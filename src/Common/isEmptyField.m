0f868d038f95a6eb1312dd12416fe637d287122a
Agah <agahkarakuzu@gmail.com>
2021-10-14 01:34:45 -0400

    Refactoring the function for Juan

--- src/Common/isEmptyField.m
+++ src/Common/isEmptyField.m
@@ -1,8 +1,23 @@
-%isEmptyField function
-%Checks if a field of a structure exists AND if that field is not empty
-function output = isEmptyField(structName,fieldName)
-    if isfield(structName,fieldName) && ~isempty(structName.(fieldName))
+% Helper function to query whether a struct field is empty 
+% provided that the field exists (false otherwise). 
+% 
+% Inputs:
+%   inStruct        (type struct) The input struct       
+%   fieldName       (type string) The field name for which the 
+%                                 inStruct is queried  
+%
+% Outputs:
+%   output         (type boolean) false if the field is not empty 
+%                                 OR does not exist 
+%                                 true otherwise.       
+%
+% Written by: Juan Velazques-Reyes
+% October, 2021 
+
+function output = isEmptyField(inStruct,fieldName)
+    if isfield(inStruct,fieldName) && ~isempty(inStruct.(fieldName))
         output = false;
     else
         output = true;
     end
+end
