8c41e49af0a98602857fb884c312ae6503422b71
Kamila-Bober <kamila.bober785@gmail.com>
2023-04-06 19:31:52 -0400

    Created initial multi echo B0 map file and edited dual echo

--- src/Models/FieldMaps/b0_mem.m
+++ src/Models/FieldMaps/b0_mem.m
@@ -1,60 +1,14 @@
-classdef b0_dem < AbstractModel
-% b0_dem map :  Dual Echo Method for B0 mapping
-%
-% Assumptions:
-%   Compute B0 map based on 2 phase images with different TEs
-%
-% Inputs:
-%   Phase       4D phase image, 2 different TEs in time dimension
-%   Magn        3D magnitude image
-%
-% Outputs:
-%	B0map       B0 field map [Hz]
-%
-% Protocol:
-%   TimingTable
-%       deltaTE     Difference in TE between 2 images [ms]
-%
-% Options:
-%   Magn thresh     relative threshold for the magnitude (phase is undefined in the background
-%
-% Example of command line usage:
-%   Model = b0_dem;  % Create class from model
-%   Model.Prot.TimingTable.Mat = 1.92e-3; % deltaTE [s]
-%   data.Phase = double(load_nii_data('Phase.nii.gz'));%Load 4D data, 2 frames with different TE
-%   data.Magn  = double(load_nii_data('Magn.nii.gz'));
-%   FitResults       = FitData(data,Model);
-%   FitResultsSave_nii(FitResults,'Phase.nii.gz'); %save nii file using Phase.nii.gz as template
-%
-%   For more examples: <a href="matlab: qMRusage(b0_dem);">qMRusage(b0_dem)</a>
-%
-% Author: Ian Gagnon, 2017
-%
-% References:
-%   Please cite the following if you use this module:
-%     Maier, F., Fuentes, D., Weinberg, J.S., Hazle, J.D., Stafford, R.J.,
-%     2015. Robust phase unwrapping for MR temperature imaging using a
-%     magnitude-sorted list, multi-clustering algorithm. Magn. Reson. Med.
-%     73, 1662?1668. Schofield, M.A., Zhu, Y., 2003. Fast phase unwrapping
-%     algorithm for interferometric applications. Opt. Lett. 28, 1194?1196
-%   In addition to citing the package:
-%     Karakuzu A., Boudreau M., Duval T.,Boshkovski T., Leppert I.R., Cabana J.F., 
-%     Gagnon I., Beliveau P., Pike G.B., Cohen-Adad J., Stikov N. (2020), qMRLab: 
-%     Quantitative MRI analysis, under one umbrella doi: 10.21105/joss.02343
-
-properties (Hidden=true)
-    onlineData_url = 'https://osf.io/zkefh/download?version=5';
-end
-
-    properties
-        MRIinputs = {'Phase', 'Magn'};
+classdef b0_mem < AbstractModel
+    
+     properties
+        MRIinputs = {'Phase','Magn'};
         xnames = {'B0 (Hz)'};
         voxelwise = 0; % 0, if the analysis is done matricially
                        % 1, if the analysis is done voxel per voxel
 
         % Protocol
         % You can define a default protocol fhere.
-        Prot = struct('TimingTable',struct('Format',{'deltaTE'},'Mat', 1.92e-3));
+        Prot = struct('TimingTable',struct('Format',{{'deltaTE'}},'Mat', [1.92e-3; 2e-3]));
 
         % Model options
         buttons = {'Magn thresh',.05};
@@ -67,7 +21,7 @@ methods (Hidden=true)
 end
 
     methods
-        function obj = b0_dem
+        function obj = b0_mem
             obj.options = button2opts(obj.buttons);
             obj = UpdateFields(obj);
         end
@@ -89,8 +43,7 @@ end
 %             rmdir('tmp','s')
 %             B0.img = unwrap(B0.img,[],4);
 %             FitResult.B0map = (B0.img(:,:,:,2) - B0.img(:,:,:,1))/(obj.Prot.TimingTable.Mat*2*pi);
-
-            % size check ("ERROR MESSAGE")
+  
             sizecheck = true;
             if not(length(size(Magn)) == length(size(Phase)))
                 FitResult = "The size of the magnitude and phase don't match";
@@ -101,6 +54,7 @@ end
                     sizecheck = false;
                 end 
             end
+            
 
 
             % phase in radians check ("ERROR MESSAGE")
@@ -112,14 +66,27 @@ end
                 end
             end
 
-            if (radcheck == true && sizecheck == true)
-     
-                % 2D or 3D data ?
-                if any(size(Phase) == 1)
-                    TwoD  = true;
+            % 2D or 3D data ?
+            if any(size(Phase) == 1)
+                TwoD  = true;
+            else
+                TwoD = false;
+            end
+
+            % Makes sure the number of echo times makes sense ("ERROR
+            % MESSAGE")
+            Techocheck = true;
+            if length(obj.Prot.TimingTable.Mat)==1
+                   Techo = 0:obj.Prot.TimingTable.Mat:size(Phase,4)*obj.Prot.TimingTable.Mat;
                 else
-                    TwoD = false;
-                end
+                    if length(obj.Prot.TimingTable.Mat)== size(Phase,4)
+                        Techo = obj.Prot.TimingTable.Mat;
+                    else
+                        Techocheck = false;
+                    end
+            end 
+
+            if (radcheck == true && sizecheck == true && Techocheck == true)
 
                 % MATLAB "sunwrap" for 2D data
                 Phase_uw = Phase;
@@ -134,11 +101,27 @@ end
                         Phase_uw(:,:,:,iEcho) = laplacianUnwrap(Phase(:,:,:,iEcho), Magn>obj.options.Magnthresh);
                     end
                 end
-                FitResult.B0map = (Phase_uw(:,:,:,2) - Phase_uw(:,:,:,1))/(obj.Prot.TimingTable.Mat*2*pi);
+                %obj.Prot.TimingTable.Mat
+                Time = 0.001;
+                Techo = zeros(1,size(Phase,4));
+                for iEcho = 0:size(Phase,4)-1
+                    Techo(iEcho+1) = Time*iEcho;
+                end
+                FitResult.B0map = zeros(size(Phase, 1:2));
+                for i = 1:size(Phase, 1)
+                    for j = 1:size(Phase,2)
+                        Phase_vec= zeros(1,size(Phase,4));
+                        for iEcho = 1:size(Phase,4)-1
+                            Phase_vec(iEcho) = Phase_uw(i, j, :,iEcho);
+                    FitResult.B0map(i, j) = polyfit(Techo, Phase_vec, 0)/(2*pi);
+                        end 
+                    end 
+                end
+                
             
                 % Save unwrapped phase
                 FitResult.Phase_uw = Phase_uw;
-            end
-        end
+            end 
+        end 
     end
-end
+end 
