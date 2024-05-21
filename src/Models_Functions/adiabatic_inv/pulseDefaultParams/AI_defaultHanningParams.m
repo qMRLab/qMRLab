6571398a2ed290b2665f4ec7c70557ca9c46812a
Amie Demmans <demmansa@mcmaster.ca>
2024-05-21 14:21:31 -0400

    Default pulse parameters

---
+++ src/Models_Functions/adiabatic_inv/pulseDefaultParams/AI_defaultHanningParams.m
@@ -0,0 +1,36 @@
+function PulseOpt = AI_defaultHanningParams(PulseOpt)
+
+% Function designed to be used with the adiabatic pulse code
+% Fills in default values if they are not user-defined
+% 
+if(~isfield(PulseOpt,'beta') || isempty(PulseOpt.beta) || ~isfinite(PulseOpt.beta))
+    % Default beta value in rad/s (modulation angular frequency)
+    PulseOpt.beta = 175;       
+end
+
+if(~isfield(PulseOpt,'A0') || isempty(PulseOpt.A0) || ~isfinite(PulseOpt.A0))
+    % Peak B1 of the pulse in microTesla
+    PulseOpt.A0 = 14;       
+end
+
+if(~isfield(PulseOpt,'n') || isempty(PulseOpt.n) || ~isfinite(PulseOpt.n))
+    % Default exponent
+    PulseOpt.n = 1;       
+end
+
+if(~isfield(PulseOpt,'nSamples') || isempty(PulseOpt.nSamples) || ~isfinite(PulseOpt.nSamples))
+    % Default number of samples taken based on machine properties
+    PulseOpt.nSamples = 512;       
+end
+
+if(~isfield(PulseOpt,'Q') || isempty(PulseOpt.Q) || ~isfinite(PulseOpt.Q))
+    % Adiabaticity factor 
+    PulseOpt.Q = 4.2e-4;       
+end
+
+if(~isfield(PulseOpt,'Trf') || isempty(PulseOpt.Trf) || ~isfinite(PulseOpt.Trf))
+    % Adiabatic pulse duration (ms)
+    PulseOpt.Trf = 10/1000;       
+end
+
+return;
